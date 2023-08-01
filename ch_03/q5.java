33bf63670dfc616a71366ff8fe0aadeb5b76d33a
tanzim12911 <tanzim12911@gmail.com>
2023-08-01 13:20:08 +0000

    Aug 1, 2023, 7:20 PM

--- ch_03/q5.java
+++ ch_03/q5.java
@@ -16,19 +16,19 @@ public class q5 {
             case 0:
                 System.out.println("Sunday");
                 break;
-            case 0:
+            case 2:
                 System.out.println("Monday");
                 break;
-            case 0:
+            case 3:
                 System.out.println("Tuesday");
                 break;
-            case 0:
+            case 4:
                 System.out.println("Thursday");
                 break;
-            case 0:
+            case 5:
                 System.out.println("Friday");
                 break;
-            case 0:
+            case 6:
                 System.out.println("Saturday");
                 break;
         
