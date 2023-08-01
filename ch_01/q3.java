2b727a9c283a4bcc2f16b13087533f24379c76bf
tanzim12911 <tanzim12911@gmail.com>
2023-08-01 12:27:27 +0000

    Aug 1, 2023, 6:27 PM

---
+++ ch_01/q3.java
@@ -0,0 +1,11 @@
+package ch_01;
+
+public class q3 {
+    public static void main(String[] args) {
+        System.out.println("   J    A    V       V    A");
+        System.out.println("   J   A A    V     V    A A");
+        System.out.println("J  J  AAAAA    V   V    AAAAA");
+        System.out.println(" JJ  A     A     V     A     A");
+    }
+    
+}
