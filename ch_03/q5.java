488d98a18c2102954dc068564229cefc7d26c45d
tanzim12911 <tanzim12911@gmail.com>
2023-08-01 13:19:45 +0000

    Aug 1, 2023, 7:19 PM

---
+++ ch_03/q5.java
@@ -0,0 +1,39 @@
+package ch_03;
+
+import java.util.Scanner;
+
+public class q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter today's day: ");
+        int day = sc.nextInt();
+        System.out.print("Enter the number of days elapsed since today:");
+        int elapsed_day = sc.nextInt();
+
+        int future_day = (day + elapsed_day) % 7;
+
+        switch (future_day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 0:
+                System.out.println("Monday");
+                break;
+            case 0:
+                System.out.println("Tuesday");
+                break;
+            case 0:
+                System.out.println("Thursday");
+                break;
+            case 0:
+                System.out.println("Friday");
+                break;
+            case 0:
+                System.out.println("Saturday");
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
