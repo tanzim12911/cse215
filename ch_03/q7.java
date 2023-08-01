3c67c17edb4c9fce72a524b1ab0bed350c1bb79a
tanzim12911 <tanzim12911@gmail.com>
2023-08-01 14:19:19 +0000

    Aug 1, 2023, 8:19 PM

--- ch_03/q7.java
+++ ch_03/q7.java
@@ -14,7 +14,9 @@ public class q7 {
         int amount_quarters = (temp) / 25;
         temp %= 25;
         int amount_dimes = temp / 10;
-        int amount_pennies = temp % 10;
+        temp %= 10;
+        int amount_nickels = temp / 5;
+        int amount_pennies = temp % 5;
 
         System.out.println("Your amount " + amount + " consists of");
 
@@ -33,10 +35,15 @@ public class q7 {
         else
             System.out.println("    " + amount_dimes + " dime");
 
-        if (amount_pennies > 1)
+        if (amount_nickels > 1)
+            System.out.println("    " + amount_nickels + "nickel(s)");
+        else
+            System.out.println("    " + amount_nickels + "nickel");
+        
+            if (amount_pennies > 1)
             System.out.println("    " + amount_pennies + "pennies(s)");
         else
-            System.out.println("    " + amount_pennies + "pennie");
+            System.out.println("    " + amount_pennies + "penny");
 
     }
 }
