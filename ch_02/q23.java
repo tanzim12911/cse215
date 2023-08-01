8a11a506e4cded8051f1041d6447d123ff48c665
tanzim12911 <tanzim12911@gmail.com>
2023-08-01 13:09:51 +0000

    Aug 1, 2023, 7:09 PM

---
+++ ch_02/q23.java
@@ -0,0 +1,20 @@
+package ch_02;
+
+import java.util.Scanner;
+
+public class q23 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the driving distance: ");
+        double distance = input.nextDouble();
+        System.out.print("Enter miles per gallon: ");
+        double milesPerGallon = input.nextDouble();
+        System.out.print("Enter price per gallon: ");
+        double pricePerGallon = input.nextDouble();
+
+        double costOfDriving = (distance / milesPerGallon) * pricePerGallon;
+
+        System.out.println("The cost of driving is $" + costOfDriving);
+    }
+}
