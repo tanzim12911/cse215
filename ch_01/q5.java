a8f038f98b37272b07bc5747bb931639dff38dcd
tanzim12911 <tanzim12911@gmail.com>
2023-08-01 12:29:01 +0000

    Aug 1, 2023, 6:29 PM

---
+++ ch_01/q5.java
@@ -0,0 +1,12 @@
+package ch_01;
+
+public class q5 {
+    public static void main(String[] args) {
+        double numerator = (9.5 * 4.5) - (2.5 * 3);
+        double denominator = (45.5 -3.5);
+
+        double result = numerator / denominator;
+
+        System.out.println(result);
+    }    
+}
