5c16a7aad440a780c907c330737027aa1106e435
tanzim12911 <tanzim12911@gmail.com>
2023-08-01 12:36:50 +0000

    Aug 1, 2023, 6:36 PM

---
+++ ch_01/q8.java
@@ -0,0 +1,16 @@
+package ch_01;
+
+public class q8 {
+    public static void main(String[] args) {
+        double radius, perimeter, area, pi;
+        radius = 5.5;
+        pi = 3.1416;
+
+        perimeter = 2 * radius * pi;
+        area = radius * radius * pi;
+
+        System.out.println("Perimeter = " + perimeter);
+        System.out.println("Area = " + area);
+
+    }
+}
