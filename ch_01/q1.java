f379c97c09d92332a7526185c6f6d2bc3e161054
tanzim12911 <tanzim12911@gmail.com>
2023-08-01 12:24:04 +0000

    Aug 1, 2023, 6:24 PM

---
+++ ch_01/q1.java
@@ -0,0 +1,11 @@
+package ch_01;
+
+
+public class q1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+    
+}
