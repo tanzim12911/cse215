2fc00b7d4b4f4a17ef522f3365a33183419b0dcf
tanzim12911 <tanzim12911@gmail.com>
2023-08-01 12:26:16 +0000

    Aug 1, 2023, 6:26 PM

--- ch_01/q1.java
+++ ch_01/q1.java
@@ -1,6 +1,5 @@
 package ch_01;
 
-
 public class q1 {
     public static void main(String[] args) {
         System.out.println("Welcome to Java");
---
+++ ch_01/q2.java
@@ -0,0 +1,13 @@
+package ch_01;
+
+public class q2 {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i < 5)
+        {
+            System.out.println("Welcome to Java");
+            i++;
+        } 
+    }
+}
+       
