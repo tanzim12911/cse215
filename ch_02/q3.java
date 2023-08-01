2d59bded49b1eb6b3c4251c395033a46f5137b37
tanzim12911 <tanzim12911@gmail.com>
2023-08-01 12:54:06 +0000

    Aug 1, 2023, 6:54 PM

---
+++ ch_02/q3.java
@@ -0,0 +1,16 @@
+package ch_02;
+
+import java.util.Scanner;
+
+public class q3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a value in feet: ");
+        double feet = input.nextDouble();
+        double meters = feet * 0.305;
+
+        System.out.println(feet + " feet is " + meters + " meters");
+        
+    }
+}
