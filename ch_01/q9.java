7cf2b567a7eae1322969d51331694422c17d4fe0
tanzim12911 <tanzim12911@gmail.com>
2023-08-01 12:38:19 +0000

    Aug 1, 2023, 6:38 PM

---
+++ ch_01/q9.java
@@ -0,0 +1,14 @@
+package ch_01;
+
+public class q9 {
+    public static void main(String[] args) {
+        double width, height, area;
+
+        width = 4.5;
+        height = 7.9;
+
+        area = width * height;
+
+        System.out.println("Area = " + area);
+    }
+}
