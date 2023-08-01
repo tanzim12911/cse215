e3d4d4eab41532847a4946a37be0e3638da54787
tanzim12911 <tanzim12911@gmail.com>
2023-08-01 14:44:49 +0000

    Aug 1, 2023, 8:44 PM

---
+++ ch_03/q8.java
@@ -0,0 +1,26 @@
+package ch_03;
+import java.util.Scanner;
+
+public class q8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three integers: ");
+        int num_1 = sc.nextInt();
+        int num_2 = sc.nextInt();
+        int num_3 = sc.nextInt();
+
+        int temp;
+
+        if (num_1 < num_2); {
+            temp = num_1;
+            num_1 = num_2;
+            num_2 = temp;
+        }
+        else if (num_2 < num_3) {
+            temp = num_2;
+            num_2 = num_3;
+            num_3 = temp;
+        }
+
+    }
+}
