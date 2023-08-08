1837da27a0709882a05725aad1a8aa27f4c660d9
tanzim12911 <tanzim12911@gmail.com>
2023-08-08 13:57:08 +0000

    Aug 8, 2023, 7:57 PM

--- ch_05/q8.java
+++ ch_05/q8.java
@@ -1,5 +1,27 @@
 package ch_05;
 
+import java.util.Scanner;
+
 public class q8 {
-    
+    public static void main(String[] args) {
+        String stuName, stuNameHighest;
+        int stuScore, stuScoreHighest = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of students: ");
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++)
+        {
+            System.out.print("Enter Name of Student " + i + ": ");
+            stuName = sc.nextLine();
+            System.out.print("Enter Score of Student " + i + ": ");
+            stuScore = sc.nextInt();
+            
+            if(stuScore > stuScoreHighest)
+            {
+                stuScoreHighest = stuScore;
+                stuNameHighest = stuName;
+            }
+        }
+    }
 }
