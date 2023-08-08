b66926828540a8ea2df45ee37d8a15315698f78e
tanzim12911 <tanzim12911@gmail.com>
2023-08-08 13:02:33 +0000

    Aug 8, 2023, 7:02 PM

--- ch_05/q6.java
+++ ch_05/q6.java
@@ -8,8 +8,8 @@ public class q6 {
             double calcKM = i * 1.609;
             double calcML = j / 1.609;
             
-            System.out.print(i + "           ");
-            System.out.printf(" %.1f", calcKM);
+            System.out.print(i + "  ");
+            System.out.printf(" %8.1f", calcKM);
             System.out.print(" |    " + j + "    ");
             System.err.printf(" %.2f\n", calcML);
         }
