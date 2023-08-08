8aecf6cf3fcef9d366b1f3205af48ae648ca0a47
tanzim12911 <tanzim12911@gmail.com>
2023-08-08 09:32:52 +0000

    Aug 8, 2023, 3:32 PM

--- ch_05/q1.java
+++ ch_05/q1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class q1 {
     public static void main(String[] args) {
-        int num, sum = 0,count_pos = 0, count_neg = 0;
+        int num, sum = 0, count_num = 0, count_pos = 0, count_neg = 0;
         System.out.print("Enter an integer, the input ends if it is 0: ");
         Scanner sc = new Scanner(System.in);
         num = sc.nextInt();
@@ -15,7 +15,15 @@ public class q1 {
                 count_neg++;
 
             sum += num;
+            count_num++;
         }
+
+        double avg = sum / count_num;
+
+        System.out.println("The number of positives is " + count_pos);
+        System.out.println("The number of negatives is " + count_neg);
+        System.out.println("The total is " + sum);
+        System.out.println("The average is " + avg);
 
     }
 }
