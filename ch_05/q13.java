4e327c5e9f751a12cb6b0484d634a9d2f0a87a6e
tanzim12911 <tanzim12911@gmail.com>
2023-08-08 14:58:31 +0000

    Aug 8, 2023, 8:58 PM

--- ch_05/q13.java
+++ ch_05/q13.java
@@ -3,7 +3,7 @@ package ch_05;
 public class q13 {
     public static void main(String[] args) {
         int n = 20;
-        while (n * n * n > 12000) 
+        while (n * n * n < 12000) 
         {
             n++;
         }
