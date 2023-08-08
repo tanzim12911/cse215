8850c585389116d8c06c62a3da66439f0f5bcd98
tanzim12911 <tanzim12911@gmail.com>
2023-08-08 14:43:14 +0000

    Aug 8, 2023, 8:43 PM

---
+++ ch_05/q11.java
@@ -0,0 +1,21 @@
+package ch_05;
+
+public class q11 {
+    public static void main(String[] args) {
+        int count;
+        for (int i = 100; i <= 200; i++)
+        {
+            if (i % 5 == 0 ^ i % 6 == 0)
+            {
+                System.out.print(i + " ");
+                count++;
+
+                if (count == 10)
+                {
+                    System.out.println();
+                    count = 0;
+                }
+            }
+        }
+    }
+}
