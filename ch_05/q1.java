b1cc98d8b7f6c3b58a60fd4a5dd3e49e269c7cb3
tanzim12911 <tanzim12911@gmail.com>
2023-08-08 09:39:48 +0000

    Aug 8, 2023, 3:39 PM

--- ch_05/q1.java
+++ ch_05/q1.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class q1 {
     public static void main(String[] args) {
-        int num, sum = 0, count_num = 0, count_pos = 0, count_neg = 0;
+        int sum = 0, count_num = 0, count_pos = 0, count_neg = 0;
         System.out.print("Enter an integer, the input ends if it is 0: ");
         Scanner sc = new Scanner(System.in);
         
         while (num != 0) {
-            
+
             num = sc.nextInt();
             if (num > 0)
                 count_pos++;
