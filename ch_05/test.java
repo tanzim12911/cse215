ae3fdec85c9f140ff17b0f47ca030eef74221514
tanzim12911 <tanzim12911@gmail.com>
2023-08-08 09:41:01 +0000

    Aug 8, 2023, 3:41 PM

--- ch_05/test.java
+++ ch_05/test.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ReadIntegers {
+public class test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
