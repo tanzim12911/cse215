1bf6938862312ecb024daf5923283bbae1e1f4e4
tanzim12911 <tanzim12911@gmail.com>
2023-08-08 13:01:15 +0000

    Aug 8, 2023, 7:01 PM

--- ch_05/q6.java
+++ ch_05/q6.java
@@ -1,5 +1,17 @@
 package ch_05;
 
 public class q6 {
-    
+    public static void main(String[] args) {
+        System.out.println("Kilograms Pounds | Pounds Kilograms");
+        for (int i = 1, j = 20; i <= 10; i ++, j += 5) 
+        {
+            double calcKM = i * 1.609;
+            double calcML = j / 1.609;
+            
+            System.out.print(i + "       ");
+            System.out.printf(" %.1f", calcKM);
+            System.out.print(" |    " + j + "    ");
+            System.err.printf(" %.2f\n", calcML);
+        }
+    }
 }
