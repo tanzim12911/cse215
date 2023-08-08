62d996581e98987ddce1247dd9446c649c80eb9e
tanzim12911 <tanzim12911@gmail.com>
2023-08-08 14:24:27 +0000

    Aug 8, 2023, 8:24 PM

--- ch_05/q10.java
+++ ch_05/q10.java
@@ -2,10 +2,21 @@ package ch_05;
 
 public class q10 {
     public static void main(String[] args) {
+        int count = 0;
         for (int i = 100; i <= 1000; i++) {
-        if (i % 5 == 0 && i % 6 == 0)
-            System.out.print(i + " ");
+            if (i % 5 == 0 && i % 6 == 0)
+            {    
+                System.out.print(i + " ");
+                count++;
+
+                if (count == 10)
+                {
+                    System.out.println();
+                    count = 0;
+                }
+            }
         }
-    }
+
+}
     
 }
