41a1305cc285a1e7ac3e931308071403679ba5b2
tanzim12911 <tanzim12911@gmail.com>
2023-08-08 12:43:15 +0000

    Aug 8, 2023, 6:43 PM

--- ch_05/q5.java
+++ ch_05/q5.java
@@ -4,10 +4,10 @@ public class q5 {
     public static void main(String[] args) {
         System.out.println("Kilograms        Pounds   |   Pounds      Kilograms");
         for (int i = 1; i <= 199; i += 2) {
-            System.out.printf("%d        %.1f", i, (i * 2.2));
+            System.out.printf("%d        %.1f\n", i, (i * 2.2));
         }
         for (int i = 20; i <= 515; i += 5 ) {
-            System.out.printf("%d        %.1f", i, (i / 2.2));
+            System.out.printf("%d        %.1f\n", i, (i / 2.2));
         }
     }
 }
