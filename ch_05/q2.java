66d622bf95c51b8d4925e9eb3e9e61db0619d711
tanzim12911 <tanzim12911@gmail.com>
2023-08-08 12:12:45 +0000

    Aug 8, 2023, 6:12 PM

---
+++ ch_05/q2.java
@@ -0,0 +1,47 @@
+package ch_05;
+
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+        int correct = 0;
+        Scanner sc = new Scanner(System.in);
+
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 10; i++) {
+            
+            int num_1 = (int) (Math.random() * 15);
+            int num_2 = (int) (Math.random() * 15);
+
+            if (num_1 < num_2)
+            {
+                int temp = num_1;
+                num_1 = num_2;
+                num_2 = num_1;
+            }
+
+            System.out.print("What is " + num_1 + " - " + num_2 + " ?" );
+            int answer = sc.nextInt();
+
+            if (answer == num_1 - num_2)
+            {
+                System.out.println("You are correct!");
+                correct++;
+            }
+            else
+            {
+                System.out.println("Your incorrect! " + num_1 + " - " + num_2 + " should be " + (num_1 - num_2));
+            }
+        }
+
+        long endTime = System.currentTimeMillis();
+
+        long testTime = endTime - startTime;
+
+        System.out.println("Correct Count: " + correct);
+        System.out.println("Test time: " + testTime);
+
+
+    }
+}
