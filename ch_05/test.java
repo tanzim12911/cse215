1cf37b61d6e4b9ff9c07ed61581b89ffd70f9d60
tanzim12911 <tanzim12911@gmail.com>
2023-08-08 09:40:31 +0000

    Aug 8, 2023, 3:40 PM

---
+++ ch_05/test.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReadIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter integers (end with 0):");
+        
+        int input = scanner.nextInt();
+        while (input != 0) {
+            System.out.println("You entered: " + input);
+            input = scanner.nextInt();
+        }
+        
+        System.out.println("Program ended.");
+    }
+}
+
