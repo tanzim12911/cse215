e93d0268ef5d99b22e9d2a8cd684c4828243402c
tanzim12911 <tanzim12911@gmail.com>
2023-08-08 12:21:38 +0000

    Aug 8, 2023, 6:21 PM

---
+++ ch_05/q4.java
@@ -0,0 +1,11 @@
+package ch_05;
+
+public class q4 {
+    public static void main(String[] args) {
+        System.out.println("Miles       Kilometers");
+        for (int i = 1; i <= 199; i++) {
+            System.out.println(i + "        " + (i * 1.609));
+        }
+    }
+    }
+}
