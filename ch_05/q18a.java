2a5b8270f7ca4a15dab87dcf22d70fdf17c992a6
tanzim12911 <tanzim12911@gmail.com>
2023-08-09 06:33:48 +0000

    Aug 9, 2023, 12:33 PM

--- ch_05/q18a.java
+++ ch_05/q18a.java
@@ -2,11 +2,11 @@ package ch_05;
 
 public class q18a {
     public static void main(String[] args) {
-        for (int i = 0; i < 6; i++)
+        for (int i = 1; i <= 6; i++)
         {
-            for (int j = 0; j <= i; j++)
+            for (int j = 1; j <= i; j++)
             {
-                System.out.print(i + 1);
+                System.out.print(i + " " + (j + 1));
             }
             System.out.println();
         }
