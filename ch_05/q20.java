c24a485c1103eb85f4403565e894ce352be3ffd5
tanzim12911 <tanzim12911@gmail.com>
2023-08-09 07:05:56 +0000

    Aug 9, 2023, 1:05 PM

---
+++ ch_05/q20.java
@@ -0,0 +1,24 @@
+package ch_05;
+
+public class q20 {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 2; i <= 100; i++)
+        {
+            for (int j = 2; j <= 100; j++)
+            {
+                if (i % j == 0)
+                {
+                    System.out.print(i + " ");
+                    count++;
+
+                    if (count == 8)
+                    {
+                        System.out.println();
+                        count = 0;
+                    }
+                }
+            }
+        }
+    }
+}
