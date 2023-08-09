45dabd36e67d5900c68ed742bce3d8562a8f02b0
tanzim12911 <tanzim12911@gmail.com>
2023-08-09 07:08:01 +0000

    Aug 9, 2023, 1:08 PM

--- ch_05/q20.java
+++ ch_05/q20.java
@@ -5,9 +5,9 @@ public class q20 {
         int count = 0;
         for (int i = 2; i <= 100; i++)
         {
-            for (int j = 2; j <= 100; j++)
+            for (int j = i; j <= 100; j++)
             {
-                if (i % j == 0)
+                if (i % j != 0)
                 {
                     System.out.print(i + " ");
                     count++;
