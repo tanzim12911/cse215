c08c8eadf903407597dff242dea9ebef580f80b5
tanzim12911 <tanzim12911@gmail.com>
2023-08-15 17:39:28 +0000

    Aug 15, 2023, 11:39 PM

---
+++ ch07/q5.java
@@ -0,0 +1,31 @@
+package ch07;
+
+import java.util.Scanner;
+
+public class q5 {
+    public static void main(String[] args) {
+        int count = 0;
+        int [] numList = new int[10];
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter ten numbers: ");
+        for (int i = 0; i < 10; i++)
+        {
+            int temp = sc.nextInt();
+            for (int j = i; j >= 0; j--)
+            {
+                if (temp != numList[j]) {
+                    numList[i] = temp;
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("The number of distinct number is " + count);
+
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.print(numList[i] + " ");
+        }
+    }
+}
