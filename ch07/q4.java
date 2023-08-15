5a6ad931ad9c2393d767f6bce1655a83f1a43fea
tanzim12911 <tanzim12911@gmail.com>
2023-08-15 17:10:07 +0000

    Aug 15, 2023, 11:10 PM

---
+++ ch07/q4.java
@@ -0,0 +1,40 @@
+package ch07;
+
+import java.util.Scanner;
+
+public class q4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the scores: ");
+        double [] numList = new double[100];
+        int count = 0, countAbove = 0, countBelow = 0, countEqual = 0;
+        double sum = 0, avg;
+        do {
+            numList[count] = sc.nextDouble();
+            count++;
+        } while (numList[count] > 0);
+
+        for (int i = 0; i < count + 1; i++)
+        {
+            sum += numList[i];
+        }
+
+        avg = sum / count;
+
+        for (int i = 0; i < count + 1; i++)
+        {
+            if (numList[i] > avg)
+                countAbove++;
+            else if (numList[i] == avg)
+                countEqual++;
+            else
+                countBelow++;
+        }
+
+        System.out.println("Scores Equal to Average: " + countEqual);
+        System.out.println("Scores Above Average: " + countAbove);
+        System.out.println("Scores Below Average: " + countBelow);
+
+
+    }
+}
