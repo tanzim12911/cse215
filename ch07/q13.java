883be09aaa026d33c3e2fdf992a8369ea1e574bd
tanzim12911 <tanzim12911@gmail.com>
2023-08-22 15:53:12 +0000

    Aug 22, 2023, 9:53 PM

--- ch07/q13.java
+++ ch07/q13.java
@@ -3,7 +3,7 @@ package ch07;
 import java.util.Scanner;
 
 public class q13 {
-    static final int SIZE = 100;
+    static final int SIZE = 3;
     public static void main(String[] args) {
         int[] numList = new int[SIZE];
         Scanner sc = new Scanner(System.in);
