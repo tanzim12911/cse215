85b9ac3361a4d85150d88cfe8c2a1317adfc5910
tanzim12911 <tanzim12911@gmail.com>
2023-08-22 14:41:19 +0000

    Aug 22, 2023, 8:41 PM

---
+++ ch07/q10.java
@@ -0,0 +1,36 @@
+package ch07;
+
+import java.util.Scanner;
+
+public class q10 {
+    public static void main(String[] args) {
+        double[] numList = new double[10];
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 10 numbers: ");
+        
+        for (int i = 0; i < 10; i++) {
+            numList[i] = sc.nextDouble();
+        }
+
+        if (numList.length > 1)
+            System.out.println("The minumum number is: " + min(numList));
+        else
+            System.out.println("Error: The number of element in the array is 1");
+
+    }
+
+    public static double indexOfSmallestElement(double[] array) {
+        double numMin = array[0];
+        int numMinIndex = 0;
+
+        for (int i = 1; i < array.length; i++) {
+
+            if(numMin > array[i]) {
+                numMin = array[i];
+                numMinIndex = i;
+            }
+        }
+        return numMinIndex;
+    }
+
+}
