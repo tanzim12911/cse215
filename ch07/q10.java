aafacafffd03f586e949acef44ada4647d170a41
tanzim12911 <tanzim12911@gmail.com>
2023-08-22 14:42:38 +0000

    Aug 22, 2023, 8:42 PM

--- ch07/q10.java
+++ ch07/q10.java
@@ -13,7 +13,7 @@ public class q10 {
         }
 
         if (numList.length > 1)
-            System.out.println("The minumum number is: " + indexOfSmallestElement(numList));
+            System.out.println("The index of minumum number is: " + indexOfSmallestElement(numList));
         else
             System.out.println("Error: The number of element in the array is 1");
 
