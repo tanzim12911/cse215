8599181f4c48c4a874084a2bd7e3ae219b51a236
tanzim12911 <tanzim12911@gmail.com>
2023-08-22 15:43:09 +0000

    Aug 22, 2023, 9:43 PM

--- ch07/q13.java
+++ ch07/q13.java
@@ -7,7 +7,7 @@ public class q13 {
     public static void main(String[] args) {
         int[] numList = new int[SIZE];
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter numbers to exclude (1 to 54):");
+        System.out.print("Enter numbers to exclude (1 to 54): ");
         for (int i = 0; i < numList.length; i++ ) {
             numList[i] = sc.nextInt();
         }
