91b36ed1df2a71e82074b51ef9d4f0eec32aa50c
tanzim12911 <tanzim12911@gmail.com>
2023-08-25 18:04:09 +0000

    Aug 26, 2023, 12:04 AM

--- test.java
+++ test.java
@@ -1,8 +1,6 @@
-package assignment_01;
-
 import java.util.Scanner;
 
-public class q3 {
+public class test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
