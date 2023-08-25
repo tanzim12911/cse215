e3eabbb33ffb5c15faebecc7540fd49fdcf7c7e6
tanzim12911 <tanzim12911@gmail.com>
2023-08-25 17:37:09 +0000

    Aug 25, 2023, 11:37 PM

--- assignment_01/q2.java
+++ assignment_01/q2.java
@@ -7,6 +7,8 @@ public class q2 {
         Scanner sc = new Scanner(System.in);
         
         int mat[][] = new int[5][5];
+        int sumMajor = 0;
+        int sumMinor = 0;
         
         System.out.println("Enter a 5-by-5 matrix row by row:");
         
@@ -16,13 +18,17 @@ public class q2 {
             }
         }
 
-        System.out.println("lol");
         for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(mat[i][j] + " ");
-            }
-            System.out.println();
+            sumMajor += mat[i][i];
         }
+        System.out.println("Sums of the elements in the major diagonal is " + sumMajor);
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j >= 0; j--) {
+                sumMinor += mat[i][j];
+            }
+        }
+        System.out.println("Sums of the elements in the minor diagonal is " + sumMinor);
     }
     
 }
