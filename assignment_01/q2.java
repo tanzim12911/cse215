317a6ab44d9c1c887defb1b2c53ee13c02a6ef1e
tanzim12911 <tanzim12911@gmail.com>
2023-08-25 17:27:04 +0000

    Aug 25, 2023, 11:27 PM

---
+++ assignment_01/q2.java
@@ -0,0 +1,27 @@
+package assignment_01;
+
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int mat[][] = new int[5][5];
+        
+        System.out.println("Enter a 5-by-5 matrix row by row:");
+        
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
+    }
+    
+}
