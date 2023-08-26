f0983df4c41b0ec5897426efbc23461083ec5982
tanzim12911 <tanzim12911@gmail.com>
2023-08-26 11:15:16 +0000

    Aug 26, 2023, 5:15 PM

---
+++ assignment_01/q4.java
@@ -0,0 +1,37 @@
+package assignment_01;
+
+import java.util.Scanner;
+
+public class q4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your password: ");
+        String pass = sc.nextLine();
+
+        boolean status = ;
+
+        if (status)
+            System.out.println("Password is Valid");
+        else
+            System.out.println("Password is invalid");
+    }
+
+    static boolean isValidPassword(String password) {
+        char last_char = password.charAt(password.length() - 1);
+        if (password.length() >= 6) 
+        {
+            if (java.lang.Character.isDigit(password.charAt(password.length() - 1)))
+            {
+                for (int i = 0; i < password.length(); i++) 
+                {
+                    if (java.lang.Character.isLetter(i)) 
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        else
+            return false;
+    }
+} 
