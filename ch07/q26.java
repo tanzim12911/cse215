54b9a6617c679470e2c11c5e966ae748b58b7a8c
tanzim12911 <tanzim12911@gmail.com>
2023-08-27 14:19:58 +0000

    Aug 27, 2023, 8:19 PM

--- ch07/q26.java
+++ ch07/q26.java
@@ -7,19 +7,19 @@ public class q26 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter list 1: ");
         int size1 = sc.nextInt();
-        int[] list1 = new int[size1];
+        int[] list1 = new int[size1 + 1];
         list1[0] = size1;
 
-        for (int i = 1; i < size1; i++) {
+        for (int i = 1; i < list1.length; i++) {
             list1[i] = sc.nextInt();
         }
 
         System.out.print("Enter list 2: ");
         int size2 = sc.nextInt();
-        int[] list2 = new int[size2];
+        int[] list2 = new int[size2 + 1];
         list2[0] = size2;
 
-        for (int i = 1; i < size2; i++) {
+        for (int i = 1; i < list2.length; i++) {
             list2[i] = sc.nextInt();
         }
 
