f21735972b71d2e36bb1992cbaee65189a8603ed
tanzim12911 <tanzim12911@gmail.com>
2023-08-27 14:34:14 +0000

    Aug 27, 2023, 8:34 PM

---
+++ ch07/q28.java
@@ -0,0 +1,21 @@
+package ch07;
+
+import java.util.Scanner;
+
+public class q28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 10 integers: ");
+        int [] numList = new int[10];
+        for (int i = 0; i < 10; i++) {
+            numList[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println("All combination of " + numList[i] + " and " + numList[i + 1]);
+            System.out.println(numList[i] + " " + numList[i + 1]);
+            System.out.println(numList[i + 1] + " " + numList[i]);
+        }
+    }
+}
