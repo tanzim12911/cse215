2bc4bd45d1b3b959e780bdcc93e5120582d1b082
tanzim12911 <tanzim12911@gmail.com>
2023-08-27 06:44:57 +0000

    Aug 27, 2023, 12:44 PM

---
+++ ch07/q15.java
@@ -0,0 +1,19 @@
+package ch07;
+
+import java.util.Scanner;
+
+public class q15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numList = new int[10];
+        System.out.print("Enter ten numbers: ");
+        for (int i = 0; i < 10; i++) {
+            numList[i] =sc.nextInt();
+        }
+    }
+
+    public static int[] eliminateDuplicates(int[] list) {
+        
+    }
+
+}
