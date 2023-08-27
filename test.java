4a4782ca3245b0911a7b97e85bab4769c6f4d6d9
tanzim12911 <tanzim12911@gmail.com>
2023-08-27 12:45:56 +0000

    Aug 27, 2023, 6:45 PM

--- test.java
+++ test.java
@@ -4,7 +4,7 @@ public class test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Enter the number of courses: ");
+        
 
         
         String[] gradeList = new String[4];
