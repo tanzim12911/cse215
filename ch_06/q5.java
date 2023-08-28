9ec72cbb2a149498b98f20e34922d874cbc96cab
tanzim12911 <tanzim12911@gmail.com>
2023-08-28 17:38:31 +0000

    Aug 28, 2023, 11:38 PM

---
+++ ch_06/q5.java
@@ -0,0 +1,22 @@
+package ch_06;
+
+import java.util.Scanner;
+
+public class q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+
+
+    }
+
+    public static void displaySortedNumbers(double num1, double num2, double num3) {
+        double temp = (num1 > num1) ? num1 : num2;
+        double max = (temp > num3) ? temp : num3;
+
+        System.out.println(max + temp + (num1 + num2 + num3 - max - temp));
+    }
+}
