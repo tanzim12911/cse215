f87d6c8abdd99fba83293241f214446aef8b6b16
tanzim12911 <tanzim12911@gmail.com>
2023-08-28 03:09:18 +0000

    Aug 28, 2023, 9:09 AM

--- ch_08/q10.java
+++ ch_08/q10.java
@@ -11,6 +11,38 @@ public class q10 {
             }
             System.out.println();    
         }
+    
+        int[] col_sum = new int[4];
+        int col_index = 0;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                col_sum[i] += mat[j][i];
+            }    
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (col_sum[i] < col_sum[i + 1])
+                col_index = i + 1;
+        }
+        
+        int[] row_sum = new int[4];
+        int row_index = 0;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                row_sum[i] += mat[i][j];
+            }    
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (row_sum[i] < row_sum[i + 1])
+                row_index = i + 1;
+        }
+
+        System.out.println("The largest row index: " + row_index);
+        System.out.println("The largest column index: " + col_index);
+        
     }
 }
 
