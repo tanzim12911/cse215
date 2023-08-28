7fffef7b28e20340b6f96a15c229c984e0009d3a
tanzim12911 <tanzim12911@gmail.com>
2023-08-28 17:26:27 +0000

    Aug 28, 2023, 11:26 PM

--- ch_06/q3.java
+++ ch_06/q3.java
@@ -18,7 +18,7 @@ public class q3 {
         
         while (number != 0) {
             int remainder = number % 10;
-            reverse *= 10 + remainder;
+            reverse = reverse * 10 + remainder;
             number /= 10;
         }
 
