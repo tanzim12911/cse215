9d282d4136e1aeaa63dcf12af1669f5aff38be94
tanzim12911 <tanzim12911@gmail.com>
2023-08-28 01:53:01 +0000

    Aug 28, 2023, 7:53 AM

--- ch_08/q1.java
+++ ch_08/q1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class q1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] mat = new int[3][4]
+        int[][] mat = new int[3][4];
         System.out.println("Enter a 3-by-4 matrix row by row: ");
         for (int i = 0; i < 3; i++) {
             for (int j =0; j < 4; j++) {
