13544a6a06af98781c32642f626a99c9e97793a5
tanzim12911 <tanzim12911@gmail.com>
2023-08-28 17:40:07 +0000

    Aug 28, 2023, 11:40 PM

--- ch_06/q5.java
+++ ch_06/q5.java
@@ -14,7 +14,7 @@ public class q5 {
     }
 
     public static void displaySortedNumbers(double num1, double num2, double num3) {
-        double temp = (num1 > num1) ? num1 : num2;
+        double temp = (num1 > num2) ? num1 : num2;
         double max = (temp > num3) ? temp : num3;
 
         System.out.println(max + " " + temp + " " + (num1 + num2 + num3 - max - temp));
