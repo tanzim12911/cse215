7e14e79dbc7bb55fc20dabcf61f56b5c0f774fb5
tanzim12911 <tanzim12911@gmail.com>
2023-08-28 17:24:58 +0000

    Aug 28, 2023, 11:24 PM

---
+++ ch_06/q3.java
@@ -0,0 +1,38 @@
+package ch_06;
+
+import java.util.Scanner;
+
+public class q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = sc.nextInt();
+        if(isPalindrome(num))
+            System.out.println(num + " is a palindrome");
+        else
+            System.out.println(num + " is not a palindrome");
+    }
+
+    public static int reverse(int number) {
+        int reverse = 0;
+        
+        while (number != 0) {
+            int remainder = number % 10;
+            reverse *= 10 + remainder;
+            number /= 10;
+        }
+
+        return reverse;
+    }
+    
+    public static boolean isPalindrome(int number) {
+        boolean status = false;
+
+        if (reverse(number) == number)
+            status = true;
+        else
+            status = false;
+
+        return status;
+    }
+}
