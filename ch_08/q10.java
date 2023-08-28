c63e94dba6c0180326052d98f15f123648285432
tanzim12911 <tanzim12911@gmail.com>
2023-08-28 02:48:25 +0000

    Aug 28, 2023, 8:48 AM

--- ch_08/q10.java
+++ ch_08/q10.java
@@ -2,7 +2,7 @@ package ch_08;
 
 public class q10 {
     public static void main(String[] args) {
-        int[][] mat = new int[4][4]
+        int[][] mat = new int[4][4];
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
