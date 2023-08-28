e0c61996e52a65b234a2190316089c5ee9f4c86a
tanzim12911 <tanzim12911@gmail.com>
2023-08-28 01:57:34 +0000

    Aug 28, 2023, 7:57 AM

---
+++ ch_08/q2.java
@@ -0,0 +1,28 @@
+package ch_08;
+
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[][] mat = new double[4][4];
+        System.out.println("Enter a 4-by-4 matrix row by row: ");
+        for (int i = 0; i < 3; i++) {
+            for (int j =0; j < 4; j++) {
+                mat[i][j] = sc.nextDouble();
+            }
+        }
+
+        System.out.println("Sum of the elements in the major diagonal is " + sumMajorDiagonal(mat));
+    }
+
+    public static double sumMajorDiagonal(double[][] m) {
+        double sum = 0;
+
+        for (int i = 0; i < m.length; i++) {
+            sum += m[i][i];
+        }
+
+        return sum;
+    }
+}
