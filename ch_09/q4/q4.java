537b556b5aee902b3f568c0db7d9fd7f56d97656
tanzim12911 <tanzim12911@gmail.com>
2023-08-29 04:14:13 +0000

    Aug 29, 2023, 10:14 AM

--- ch_09/q4/q4.java
+++ ch_09/q4/q4.java
@@ -3,8 +3,11 @@ package ch_09.q4;
 import java.util.Random;
 
 public class q4 {
-    Random ran1 = new Random(1000);
-    for (int i = 0; i < 50; i++) {
+    public static void main(String[] args) {
+        Random ran1 = new Random(1000);
+        for (int i = 0; i < 50; i++) {
         System.out.print(ran1.nextInt(100) + " ");
+        }
     }
+    
 }
