1d2d87782142edce8714ff3e54e1992c082f6d02
tanzim12911 <tanzim12911@gmail.com>
2023-08-29 05:34:14 +0000

    Aug 29, 2023, 11:34 AM

--- ch_09/q5/q5.java
+++ ch_09/q5/q5.java
@@ -1,13 +1,16 @@
 package ch_09.q5;
 
 import java.util.GregorianCalendar;
+import java.util.Calendar;
 
 public class q5 {
     public static void main(String[] args) {
         GregorianCalendar gcal = new GregorianCalendar();
         System.out.println("Current day: " + gcal.getTime());
-        gcal.setTimeInMillis(1234567898765L);
-        
+        int year = gcal.get(Calendar.YEAR);
+        int month = gcal.get(Calendar.MONTH);
+        int day = gcal.get(Calendar.DAY_OF_MONTH);
+        System.out.println(year + " " + month + " " + day);
     }
 
     
