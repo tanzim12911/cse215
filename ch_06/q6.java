da601a071576e68715c8fdc50dced7abed370cc5
tanzim12911 <tanzim12911@gmail.com>
2023-08-29 00:50:01 +0000

    Aug 29, 2023, 6:50 AM

---
+++ ch_06/q6.java
@@ -0,0 +1,18 @@
+package ch_06;
+
+import java.util.Scanner;
+
+public class q6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of n: ");
+        int num = sc.nextInt();
+        displayPattern(num);
+    }
+
+    public static void displayPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for ()
+        }
+    }
+}
---
+++ ch_06/q7.java
@@ -0,0 +1,24 @@
+package ch_06;
+
+import java.util.Scanner;
+
+public class q7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter investment amount: ");
+        double amount = sc.nextDouble();
+        System.out.print("Enter interest rate: ");
+        double interest = sc.nextDouble();
+
+        for (int i = 1; i <= 30; i++) {
+            futureInvestmentValue(amount, interest, i);
+        }
+    }
+
+    public static double futureInvestmentValue(double investmentAmount, double monthlyInterestRate, int years) {
+        monthlyInterestRate /= 100;
+        return investmentAmount * Math.pow((1 + monthlyInterestRate), (years * 12));
+    }
+}
+
+
