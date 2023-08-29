85e65a985ec0c1df1b9cdfafc0a9d821b58c33df
tanzim12911 <tanzim12911@gmail.com>
2023-08-29 04:05:24 +0000

    Aug 29, 2023, 10:05 AM

---
+++ ch_09/q3/q3.java
@@ -0,0 +1,13 @@
+package ch_09.q3;
+
+import java.util.Date;
+
+public class q3 {
+    public static void main(String[] args) {
+        
+        for (long i = 10000; i <= 100000000000L; i *= 10) {
+            Date date1 = new Date(i);
+            System.out.println("Elasped time since " + i + " is " + date1.toString());
+        }
+    }
+}
