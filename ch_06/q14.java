b79e9e82c682c9a74e92cdeb7ecc1748cbb6aada
tanzim12911 <tanzim12911@gmail.com>
2023-08-29 01:23:57 +0000

    Aug 29, 2023, 7:23 AM

---
+++ ch_06/q14.java
@@ -0,0 +1,16 @@
+package ch_06;
+
+public class q14 {
+    public static void main(String[] args) {
+        System.out.println("i               m(i)");
+        System.out.println("____________________");
+        for (int i = 1; i <= 901; i += 100) {
+            System.out.println(i + "               " + estimatePI(i));
+        }
+    }
+    
+    public static double estimatePI(int i) {
+        double m = (double) Math.pow(-1, i + 1) / (2 * i - 1);
+        return m;
+    }
+}
