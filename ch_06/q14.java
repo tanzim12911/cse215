aec36e73aac1d8e80271f33d2747d418a698052f
tanzim12911 <tanzim12911@gmail.com>
2023-08-29 01:24:28 +0000

    Aug 29, 2023, 7:24 AM

--- ch_06/q14.java
+++ ch_06/q14.java
@@ -10,7 +10,7 @@ public class q14 {
     }
     
     public static double estimatePI(int i) {
-        double m = (double) Math.pow(-1, i + 1) / (2 * i - 1);
+        double m = Math.pow(-1, i + 1) / (2 * i - 1);
         return m;
     }
 }
