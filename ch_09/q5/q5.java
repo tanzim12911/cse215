a55520703df133befcf8e45d7ee262b6a388a53f
tanzim12911 <tanzim12911@gmail.com>
2023-08-29 05:27:40 +0000

    Aug 29, 2023, 11:27 AM

---
+++ ch_09/q5/q5.java
@@ -0,0 +1,12 @@
+package ch_09.q5;
+
+import java.util.GregorianCalendar;
+
+public class q5 {
+    public static void main(String[] args) {
+        GregorianCalendar gcal = new GregorianCalendar();
+        System.out.println("Current day: " + gcal.getTime() );
+    }
+
+    
+}
