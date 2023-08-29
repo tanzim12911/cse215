092e2d6fad943bfa1fbf805a88f54f1c7dfb7326
tanzim12911 <tanzim12911@gmail.com>
2023-08-29 03:26:47 +0000

    Aug 29, 2023, 9:26 AM

--- ch_09/q1/q1.java
+++ ch_09/q1/q1.java
@@ -5,9 +5,14 @@ public class q1 {
         Rectangle rec1 = new Rectangle(4, 40);
         Rectangle rec2 = new Rectangle(3.5, 35.5);
 
-        System.out.println("Width: " + rec1.width);
-        System.out.println("height: " + rec1.height);
-        System.out.println("Area: " + rec1.getArea());
-        System.out.println("Perimeter: " + rec1.getPerimeter());
+        System.out.println("Width R1: " + rec1.width);
+        System.out.println("Height R1: " + rec1.height);
+        System.out.println("Area R1: " + rec1.getArea());
+        System.out.println("Perimeter R1: " + rec1.getPerimeter());
+
+        System.out.println("Width R2: " + rec2.width);
+        System.out.println("Height R2: " + rec2.height);
+        System.out.println("Area R2: " + rec2.getArea());
+        System.out.println("Perimeter R2: " + rec2.getPerimeter());
     }
 }
