1cd0fbd3c67b27f4f90bcd657223ec7774c3433c
tanzim12911 <tanzim12911@gmail.com>
2023-09-10 05:22:54 +0000

    Sep 10, 2023, 11:22 AM

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ConsecutiveNumbers {
+public class test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
