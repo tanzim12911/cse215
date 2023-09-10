834e439e61af46e4e3cd036d00f00920c5f372d9
tanzim12911 <tanzim12911@gmail.com>
2023-09-10 02:07:08 +0000

    Sep 10, 2023, 8:07 AM

--- ch_06/q10.java
+++ ch_06/q10.java
@@ -2,10 +2,23 @@ package ch_06;
 
 public class q10 {
     public static void main(String[] args) {
-        
+        int n = 10000;
+        int count = 0;
+
+        for (int i = 1; i <= 10000; i++) {
+            isPrime(i);
+            count++;
+        }
+
+        System.out.println(count);
     }
 
     public static boolean isPrime(int number) {
-        if 
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
     }
 }
