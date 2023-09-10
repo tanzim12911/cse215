6ec2dc785f0edd8fc0a7472d960fb7f707df1536
tanzim12911 <tanzim12911@gmail.com>
2023-09-10 02:20:18 +0000

    Sep 10, 2023, 8:20 AM

---
+++ ch_06/q36.java
@@ -0,0 +1,18 @@
+package ch_06;
+
+import java.util.Scanner;
+
+public class q36 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of sides: ");
+        int noOfSides = sc.nextInt();
+        System.out.print("Enter the side: ");
+        double side = sc.nextDouble();
+        System.out.println("The area of the pentagon is " + area(side));
+    }
+
+    public static double area(int n, double side) {
+        return (n * Math.pow(side, 2)) / (4.0 * Math.tan(Math.PI / n));
+    }
+}
