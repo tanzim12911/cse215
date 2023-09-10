48041b1cbe754f3f9acd7f64bf90d06f9b3e4a70
tanzim12911 <tanzim12911@gmail.com>
2023-09-10 05:30:26 +0000

    Sep 10, 2023, 11:30 AM

--- lab_04/task4.java
+++ lab_04/task4.java
@@ -5,51 +5,27 @@ import java.util.Scanner;
 public class task4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+       
         System.out.print("Enter size: ");
         int size = sc.nextInt();
+        
         int[] array = new int[size];
+        
         System.out.println("Enter numbers: ");
         for (int i = 0; i < size; i++) {
             array[i] = sc.nextInt();
         }
-
-        int[] result = indexOfThreeConsecutive(array);
-
+        
         System.out.println("Output: ");
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i] + " ");
-        }
-        
-    }
 
-    static int[] indexOfThreeConsecutive(int[] a) {
-        int count = 1;
-        int num = a[0];
-        int[] temp = new int[10];
-        int x = 0;
-
-        for (int i = 1; i < a.length; i++) {
-            if (num == a[i]) {
-                count++;
-            }
-            else {
-                num = a[i];
-                count = 1;
-            }
-
-            if(count == 3) {
-                    temp[x] = i;
-                    x++;
+        for (int i = 0; i < array.length - 3; i++) {
+            for (int j = 0; j < array.length - 2; j++) {
+                if (array[j] == array [j + 1] && array[j + 1] == array[j + 2]) {
+                    System.out.print(array[j] + " ");
                 }
+            }
         }
         
-        /*int[] result = new int[x];
         
-        for (int i = 0; i < x; i++) {
-            result[i] = temp[i];
-        }*/
-
-
-        return temp;
     }
 }
