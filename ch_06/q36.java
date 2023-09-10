3f66a450bdf17323aa1969a00b97b5e069674c59
tanzim12911 <tanzim12911@gmail.com>
2023-09-10 02:21:02 +0000

    Sep 10, 2023, 8:21 AM

--- ch_06/q36.java
+++ ch_06/q36.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class q36 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of sides: ");
+        System.out.print("Enter the number of sides: ");
         int noOfSides = sc.nextInt();
         System.out.print("Enter the side: ");
         double side = sc.nextDouble();
