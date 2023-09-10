bb9052e4ce3accece034a65769bfead1dc08145e
tanzim12911 <tanzim12911@gmail.com>
2023-09-10 02:32:23 +0000

    Sep 10, 2023, 8:32 AM

---
+++ lab_05/task1.java
@@ -0,0 +1,34 @@
+package lab_05;
+
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your string: ");
+        String str = sc.nextLine();
+        System.out.println("Number of vowels: " + countVowels(str));
+    }
+
+    public static int countVowels(String yourString) {
+        int count = 0;
+
+        for(int i = 0; i <= yourString.length(); i++) {
+            if (isVowel(yourString.charAt(i)))
+                count++;
+        }
+
+        return count;
+    }
+
+    public static boolean isVowel(char c) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o'
+        || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O'
+        || c == 'U') {
+            return true;
+        }
+
+        return false;
+    }
+}
