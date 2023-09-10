09888f39246bd602367871926ffdb19d82acfb6c
tanzim12911 <tanzim12911@gmail.com>
2023-09-10 03:43:52 +0000

    Sep 10, 2023, 9:43 AM

--- lab_04/task3.java
+++ lab_04/task3.java
@@ -21,7 +21,7 @@ public class task3 {
         // sum col
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                sum_col[i] += mat[i][j];
+                sum_col[i] += mat[j][i];
             }
         }
         for(int i = 0; i < 3; i++) {
