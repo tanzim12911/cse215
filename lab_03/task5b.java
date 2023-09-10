e1f0df94c6ba94ebd18cfb96f2daa03f7a9c567a
tanzim12911 <tanzim12911@gmail.com>
2023-09-10 11:23:29 +0000

    Sep 10, 2023, 5:23 PM

--- lab_03/task5b.java
+++ lab_03/task5b.java
@@ -5,6 +5,12 @@ public class task5b {
         int upper = (int) (1 + Math.random() * (1000 - 1 + 1));
         int lower = (int) (1 + Math.random() * (1000 - 1 + 1));
 
+        if (upper < lower) {
+            int temp = upper;
+            upper = lower;
+            lower = temp;
+        }
+
         int i = lower;
         while(i <= upper) {
             if(i % 5 == 0 || i % 8 == 0)
