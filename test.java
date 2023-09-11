2ceedfa296b7c3518b7e8f169042bacaf3717a55
tanzim12911 <tanzim12911@gmail.com>
2023-09-11 02:02:55 +0000

    Sep 11, 2023, 8:02 AM

--- test.java
+++ test.java
@@ -2,26 +2,7 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Read the size of the array
-        System.out.print("Enter size: ");
-        int size = scanner.nextInt();
-        int[] numbers = new int[size];
-
-        // Read the numbers into the array
-        System.out.print("Enter numbers: ");
-        for (int i = 0; i < size; i++) {
-            numbers[i] = scanner.nextInt();
-        }
-
-        // Iterate through the array and check for consecutive groups of 3
-        for (int i = 0; i < size - 2; i++) {
-            if (numbers[i] == numbers[i + 1] - 1 && numbers[i] == numbers[i + 2] - 2) {
-                System.out.print(numbers[i] + " ");
-            }
-        }
-
-        scanner.close();
+        Scanner sc = new Scanner(System.in);
+        
     }
 }
