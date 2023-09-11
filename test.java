2c3e70f5e2641e680ba348f7ba6fce4979b8661c
tanzim12911 <tanzim12911@gmail.com>
2023-09-11 02:30:06 +0000

    Sep 11, 2023, 8:30 AM

--- test.java
+++ test.java
@@ -4,5 +4,31 @@ public class test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        
+        int[][] mat = new int[n][n];
+        int sumOfAll = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = sc.nextInt();
+                sumOfAll += mat[i][j];
+            }
+        }
+
+        
+
+        
+        
+        
+        
+        
+        int sumOfelementsUpperD = 0;
+        int x = n - 1;
+        for (int i = 0; i < n; i++, x--) {
+            for (int j = x; j >= 0; j--) {
+                sumOfelementsUpperD += mat[i][j];
+            }
+        }
     }
 }
