4c8687cab9c8b8f835a141b144a74fcaf954fdca
tanzim12911 <tanzim12911@gmail.com>
2023-09-11 03:27:05 +0000

    Sep 11, 2023, 9:27 AM

--- ch_07/q27.java
+++ ch_07/q27.java
@@ -7,7 +7,7 @@ public class q27 {
         int i = 12;
             
     while (count <= 100) {
-            if (isPrime(i) && isPrime(reverseNonPalindrome(i))) {
+            if (isPrime(i) && isPrime(reverse(i)) && (i != reverse(x))) {
                 System.out.print(i + " ");
                 count++;
                 x++;
@@ -44,12 +44,5 @@ public class q27 {
         }
 
         return reverse;
-    }
-
-    static int reverseNonPalindrome(int x) {
-        int n = 0;
-        if (x != reverse(x))
-            n = x;
-        return n;
     }
 }
