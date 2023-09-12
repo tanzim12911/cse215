3087520c00883e632683713e3bebead91fdb721d
tanzim12911 <tanzim12911@gmail.com>
2023-09-12 03:10:57 +0000

    Sep 12, 2023, 9:10 AM

--- ch_09/q13/q13.java
+++ ch_09/q13/q13.java
@@ -21,6 +21,11 @@ public class q13 {
 
         Location location = Location.locateLargest(array);
 
+        /*To address the question of why: In Java, when you declare something as static, 
+        you are saying that it is a member of the class, not the object (hence why there is only one). 
+        Therefore it doesn't make sense to access it on the object, because that particular data member 
+        is associated with the class. */
+
         System.out.println("The location of the largest element is " + location.maxValue + " at (" + location.row + ", " + location.col + ")");
 
     }
