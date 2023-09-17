d1574e6b8b1f4641d4e9292f926d5b0558ffc5fc
tanzim12911 <tanzim12911@gmail.com>
2023-09-17 13:14:20 +0000

    Sep 17, 2023, 7:14 PM

---
+++ ch_11/q1/q1.java
@@ -0,0 +1,11 @@
+package ch_11.q1;
+
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter three sides of the triangle: ");
+    }
+}
