ec5b41a1b26805f2b789f3fe9bcda1975af7318c
tanzim12911 <tanzim12911@gmail.com>
2023-09-17 14:50:02 +0000

    Sep 17, 2023, 8:50 PM

---
+++ ch_11/q3/SavingsAccount.java
@@ -0,0 +1,16 @@
+package ch_11.q3;
+
+import ch_09.q7.Account;
+
+public class SavingsAccount extends Account {
+
+    public double minBalance;
+
+    SavingsAccount() {
+        minBalance = 0;
+    }
+
+    SavingsAccount() {
+        super(id, balance);
+    }
+}
