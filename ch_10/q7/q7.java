6886cb79321be22eaf8a9f96443039f595d78e19
tanzim12911 <tanzim12911@gmail.com>
2023-09-18 15:58:38 +0000

    Sep 18, 2023, 9:58 PM

--- ch_10/q7/q7.java
+++ ch_10/q7/q7.java
@@ -1,11 +1,30 @@
 package ch_10.q7;
 
+import java.util.Scanner;
 import ch_09.q7.Account;
 
 public class q7 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Account[] accountList = new Account[10];
         
+        Account[] accounts = createAccounts(accountList);
+
+        while(true) {
+            System.out.print("Enter an id: ");
+            int id = sc.nextInt();
+
+            if (id >= 0 && id <= 9) {
+                menu(accounts[id]);
+            }
+            else {
+                System.out.println("Incorrect id. Try again.");
+                System.out.print("Enter an id: ");
+                id = sc.nextInt();
+            }
+
+
+        }
         
     }
 
@@ -13,5 +32,37 @@ public class q7 {
         for(int i = 0; i < acc.length; i++) {
             acc[i] = new Account(i, 100);
         }
+
+        return acc;
+    }
+
+    public static void menu(Account a) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        do {
+            
+            System.out.println("Main menu");
+            System.out.println("1: check balance");
+            System.out.println("2: withdraw");
+            System.out.println("3: deposit");
+            System.out.println("4: exit");
+
+            System.out.print("Enter a choice: ");
+            n = sc.nextInt();
+
+            if (n == 1) {
+                System.out.println("Balance: " + a.getBalance());
+            }
+            else if (n == 2) {
+                System.out.println("Enter an amount to withdraw: ");
+                a.withdraw(sc.nextDouble());
+            }
+            else if (n == 3) {
+                System.out.println("Enter an amount to deposit: ");
+                a.deposit(sc.nextDouble());
+            }
+
+        } while(n != 4);
     }
 }
