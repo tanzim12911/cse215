380f420aa4de7cd249d88f0b61cfcde0ff673b5c
tanzim12911 <tanzim12911@gmail.com>
2023-09-18 08:42:12 +0000

    Sep 18, 2023, 2:42 PM

--- ch_11/q2/Student.java
+++ ch_11/q2/Student.java
@@ -7,4 +7,18 @@ public class Student extends Person {
     public static final String SOPHOMORE = "sophomore";
     public static final String SENIOR = "senior";
     public static final String JUNIOR = "junior";
+
+    Student(String status) {
+        this.STATUS = status;
+    }
+
+    Student(String name, String address, String phoneNum, String emailAdress, String status) {
+        super(name, address, phoneNum, emailAdress);
+        this.STATUS = status;
+
+    }
+
+    public String toString() {
+        return "Student.class { " + "Name= " + super.getName() + " }"; 
+    }
 }
