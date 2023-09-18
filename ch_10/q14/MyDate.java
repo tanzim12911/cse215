7103e138e16fc994f25d0368292d6a6fd4e8e181
tanzim12911 <tanzim12911@gmail.com>
2023-09-18 07:53:37 +0000

    Sep 18, 2023, 1:53 PM

---
+++ ch_10/q14/MyDate.java
@@ -0,0 +1,21 @@
+package ch_10.q14;
+
+import java.util.GregorianCalendar;
+
+public class MyDate {
+    private int year;
+    private int month;
+    private int day;
+
+    GregorianCalendar gcal= new GregorianCalendar();
+
+    MyDate() {
+        year = GregorianCalendar.YEAR;
+        month = GregorianCalendar.MONTH;
+        day = GregorianCalendar.DAY_OF_MONTH;
+    }
+
+    MyDate(long elaspedtime) {
+        gcal = 
+    }
+}
