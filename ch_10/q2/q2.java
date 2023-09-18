1bb1cf8a530256bf0ccf679a38c2fcab6b4d41c7
tanzim12911 <tanzim12911@gmail.com>
2023-09-18 13:04:29 +0000

    Sep 18, 2023, 7:04 PM

--- ch_10/q2/q2.java
+++ ch_10/q2/q2.java
@@ -2,7 +2,7 @@ package ch_10.q2;
 
 public class q2 {
     public static void main(String[] args) {
-        BMI p1 = new BMI("Tanzim", 21, 72, 5, 7);
+        BMI p1 = new BMI("Tanzim", 21, 159, 5, 7);
         System.out.println("BMI: " + p1.getBMI());
         System.out.println("Status: " + p1.getStatus());
     }
