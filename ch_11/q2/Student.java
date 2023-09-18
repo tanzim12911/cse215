1fa53bb3b86ee152e47ac0c25c0806c7b965c983
tanzim12911 <tanzim12911@gmail.com>
2023-09-18 08:06:03 +0000

    Sep 18, 2023, 2:06 PM

--- ch_11/q2/Student.java
+++ ch_11/q2/Student.java
@@ -3,4 +3,8 @@ package ch_11.q2;
 public class Student extends Person {
     protected final String STATUS;
 
+    public static final String FRESHMAN = "freshman";
+    public static final String SOPHOMORE = "sophomore";
+    public static final String SENIOR = "senior";
+    public static final String JUNIOR = "junior";
 }
