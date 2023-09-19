02ee04397f11f182b784bc1c0ceb4137c56b1170
tanzim12911 <tanzim12911@gmail.com>
2023-09-19 14:03:10 +0000

    Sep 19, 2023, 8:03 PM

---
+++ lab07/Task2/Fraction.java
@@ -0,0 +1,81 @@
+package lab07.Task2;
+
+import java.io.FileReader;
+
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    // constructors
+    Fraction() {
+    
+    }
+    Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+
+    // getter methods
+    public int getNumerator() {
+        return numerator;
+    }
+    public int getDenominator() {
+        return denominator;
+    }
+
+    // setter methods
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    // toString method
+    public String toString() {
+        return numerator + " / " + denominator;
+    }
+
+    // methods
+    public void add(Fraction fraction) {
+        int n1 = this.numerator;
+        int m1 = this.denominator;
+        int n2 = fraction.numerator;
+        int m2 = fraction.denominator;
+        
+        fraction.numerator = (n1 * m2) + (n2 * m1);
+        fraction.denominator = m1 * m2;
+    }
+
+    public void sub(Fraction fraction) {
+        int n1 = this.numerator;
+        int m1 = this.denominator;
+        int n2 = fraction.numerator;
+        int m2 = fraction.denominator;
+        
+        fraction.numerator = (n1 * m2) - (n2 * m1);
+        fraction.denominator = m1 * m2;
+    }
+
+    public void multiplication(Fraction fraction) {
+        int n1 = this.numerator;
+        int m1 = this.denominator;
+        int n2 = fraction.numerator;
+        int m2 = fraction.denominator;
+        
+        fraction.numerator = n1 * n2;
+        fraction.denominator = m1 * m2;
+    }
+
+    public void division(Fraction fraction) {
+        int n1 = this.numerator;
+        int m1 = this.denominator;
+        int n2 = fraction.numerator;
+        int m2 = fraction.denominator;
+        
+        fraction.numerator = n1 * m2;
+        fraction.denominator = m1 * n2;
+    }
+
+}
---
+++ lab07/Task2/TestClass.java
@@ -0,0 +1,13 @@
+package lab07.Task2;
+
+public class TestClass {
+    public static void main(String[] args) {
+        Fraction f1 = new Fraction(1, 4);
+        Fraction f2 = new Fraction(3, 5);
+
+        f1.toString();
+        f2.toString();
+    }
+
+    
+}
