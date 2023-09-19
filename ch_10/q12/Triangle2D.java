be013b2f0d07a64ab1b67633b7648538e193ec13
tanzim12911 <tanzim12911@gmail.com>
2023-09-19 03:05:57 +0000

    Sep 19, 2023, 9:05 AM

--- ch_10/q12/Triangle2D.java
+++ ch_10/q12/Triangle2D.java
@@ -29,6 +29,26 @@ public class Triangle2D {
 
     Triangle2D() {
         p1 = new MyPoint(0, 0);
+        p2 = new MyPoint(1, 1);
+        p3 = new MyPoint(2, 5);
+    }
+
+    Triangle2D(MyPoint p1, MyPoint p2, MyPoint p3) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+
+    public double getArea() {
+        double x1 = p1.getX();
+        double y1 = p1.getY();
+
+        double x2 = p2.getX();
+        double y2 = p2.getY();
+
+        double x3 = p3.getX();
+        double y3 = p3.getY();
         
+        return 0.5 * Math.abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
     }
 }
