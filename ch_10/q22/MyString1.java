45832aff0581f8fab1259f8205ac4a0d81553ef6
tanzim12911 <tanzim12911@gmail.com>
2023-09-19 06:33:33 +0000

    Sep 19, 2023, 12:33 PM

---
+++ ch_10/q22/MyString1.java
@@ -0,0 +1,15 @@
+package ch_10.q22;
+
+public class MyString1 {
+    char[] chars;
+    
+    public MyString1(char[] chars) {
+        this.chars = chars;
+    }
+
+    public char charAt(int index) {
+        if (chars[] != 0 && chars.length > index) {
+            return
+        }
+    }
+}
