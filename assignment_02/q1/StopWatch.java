c9081ac75f6aa7a65ad275e32ce1c67b40c8116c
tanzim12911 <tanzim12911@gmail.com>
2023-09-19 14:17:04 +0000

    Sep 19, 2023, 8:17 PM

---
+++ assignment_02/q1/StopWatch.java
@@ -0,0 +1,14 @@
+package assignment_02.q1;
+
+public class Stopwatch {
+    private long startTime;
+    private long endTime;
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+}
