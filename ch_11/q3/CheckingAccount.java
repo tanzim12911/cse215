2d9c49dcf82793a94306318fc2e53f19ca8317f0
tanzim12911 <tanzim12911@gmail.com>
2023-09-19 06:56:26 +0000

    Sep 19, 2023, 12:56 PM

--- ch_11/q3/CheckingAccount.java
+++ ch_11/q3/CheckingAccount.java
@@ -1,5 +1,16 @@
 package ch_11.q3;
 
-public class CheckingAccount {
-    
+
+public class CheckingAccount extends Account {
+    private double overDraftLimit;
+
+    CheckingAccount() {
+        overDraftLimit = 50;
+    }
+
+    CheckingAccount(int id, double balance) {
+        super(id, balance);
+    }
+
+
 }
