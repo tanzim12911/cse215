b9c5c02cf888562a1cfa550b51d96754de0a8e21
tanzim12911 <tanzim12911@gmail.com>
2023-09-19 12:52:36 +0000

    Sep 19, 2023, 6:52 PM

---
+++ ch_11/q2/Person.java
@@ -0,0 +1,34 @@
+package ch_11.q2;
+
+public class Person {
+    private String name;
+    private String address;
+    private String phoneNum;
+    private String emailAddress;
+
+    public String getName() {
+        return name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
