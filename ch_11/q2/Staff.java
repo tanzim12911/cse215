197498f860ed2bd75eda97d66a75bd5a3bbdad8b
tanzim12911 <tanzim12911@gmail.com>
2023-09-19 13:36:53 +0000

    Sep 19, 2023, 7:36 PM

---
+++ ch_11/q2/Staff.java
@@ -0,0 +1,27 @@
+package ch_11.q2;
+
+import ch_10.q14.MyDate;
+
+public class Staff extends Employee{
+    private String title;
+
+    Staff() {
+
+    }
+
+    Staff(String title) {
+        this.title = title;
+    }
+
+    Staff(String office, double salary, MyDate dateHired) {
+        
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
