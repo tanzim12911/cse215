9017a7760ddd653586882a855f9709a39d49ea8d
tanzim12911 <tanzim12911@gmail.com>
2023-09-24 12:08:05 +0000

    Sep 24, 2023, 6:08 PM

--- assignment_02/q2/q2.java
+++ assignment_02/q2/q2.java
@@ -8,12 +8,12 @@ public class q2 {
 
         System.out.println("Length of the string is: ");
         int size = sc.nextInt();
-        //char[] str = new char[size];
+        char[] str = new char[size];
 
         System.out.print("Enter your NSU ID: ");
-        
-        char[] str = {'2', '3', '2', '1', '8', '4', '2', '6', '4', '2'};
-        
+        String id = sc.next();
+
+        str = id.toCharArray();
         
         MyString1 s1 = new MyString1(str);
 
