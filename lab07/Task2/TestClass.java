bc7135eb119ac7b8e3a7879f0f8022dbf469ab8f
tanzim12911 <tanzim12911@gmail.com>
2023-09-24 08:50:00 +0000

    Sep 24, 2023, 2:50 PM

--- lab07/Task2/TestClass.java
+++ lab07/Task2/TestClass.java
@@ -9,16 +9,16 @@ public class TestClass {
         System.out.println(f2.toString());
 
         f1.add(f2);
-        System.out.println(f2.toString());
+        System.out.println(f1.toString());
 
         f1.sub(f2);
-        System.out.println(f2.toString());
+        System.out.println(f1.toString());
 
         f1.multiplication(f2);
-        System.out.println(f2.toString());
+        System.out.println(f1.toString());
 
         f1.division(f2);
-        System.out.println(f2.toString());
+        System.out.println(f1.toString());
     }
 
     
