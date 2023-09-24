02868f86675200fc1bb0e9036a95e1c9257bdfc5
tanzim12911 <tanzim12911@gmail.com>
2023-09-24 11:11:09 +0000

    Sep 24, 2023, 5:11 PM

---
+++ assignment_02/q2/q2.java
@@ -0,0 +1,14 @@
+package assignment_02.q2;
+
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your NSU ID: ");
+        
+    }
+
+    
+}
