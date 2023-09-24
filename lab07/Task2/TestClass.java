0739d1cf9ac826f2e25eb90b2748e3e4323f8bc1
tanzim12911 <tanzim12911@gmail.com>
2023-09-24 08:52:12 +0000

    Sep 24, 2023, 2:52 PM

--- lab07/Task2/TestClass.java
+++ lab07/Task2/TestClass.java
@@ -2,14 +2,22 @@ package lab07.Task2;
 
 public class TestClass {
     public static void main(String[] args) {
-        Fraction f1 = new Fraction(1, 4);
-        Fraction f2 = new Fraction(3, 5);
+        int n1 = 1;
+        int m1 = 4;
+        int n2 = 3;
+        int m2 = 5;
+
+
+        Fraction f1 = new Fraction(n1, m1);
+        Fraction f2 = new Fraction(n2, m2);
 
         System.out.println(f1.toString());
         System.out.println(f2.toString());
 
         f1.add(f2);
         System.out.println(f1.toString());
+
+        f1.setNumerator();
 
         f1.sub(f2);
         System.out.println(f1.toString());
