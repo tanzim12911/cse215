0c4849ec860727db5183852e1834017841cca761
tanzim12911 <tanzim12911@gmail.com>
2023-10-01 01:58:33 +0000

    Oct 1, 2023, 7:58 AM

--- exercise/array/PrintUniqueElements.java
+++ exercise/array/PrintUniqueElements.java
@@ -9,7 +9,7 @@ public class PrintUniqueElements {
     public static void UniqueElementsDisplay(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length; j++) {
-                if(arr[i] != arr[i + 1]) {
+                if(arr[i] != arr[j]) {
                     System.out.println(arr[i] + " ");
                 }
             }
