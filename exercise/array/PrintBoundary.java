6aae4fae82d025874ef6b577270796a95e5f40d4
tanzim12911 <tanzim12911@gmail.com>
2023-10-01 14:03:23 +0000

    Oct 1, 2023, 8:03 PM

---
+++ exercise/array/PrintBoundary.java
@@ -0,0 +1,29 @@
+package exercise.array;
+
+public class PrintBoundary {
+    public static void main(String[] args) {
+        int a[][] = { { 1, 2, 3, 4 },
+                      { 5, 6, 7, 8 },
+                      { 1, 2, 3, 4 },
+                      { 5, 6, 7, 8 } };
+        
+        PrintBoundaryMethod(a, 4, 4);
+    }
+
+    public static void PrintBoundaryMethod(int[][] array, int m, int n) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0)
+                    System.out.print(array[i][j] + " ");
+                else if (j == 0)
+                    System.out.print(array[i][j] + " ");
+                else if (j == n - 1)
+                    System.out.print(array[i][j] + " ");
+                else if (i == m - 1)
+                    System.out.print(array[i][j] + " ");
+                else
+                    System.out.print(" ");
+            }
+        }
+    }
+}
