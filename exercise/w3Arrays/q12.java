9df67f69efa442083d972497ed08c360b213130f
tanzim12911 <tanzim12911@gmail.com>
2023-10-01 13:32:22 +0000

    Oct 1, 2023, 7:32 PM

--- exercise/w3Arrays/q12.java
+++ exercise/w3Arrays/q12.java
@@ -6,7 +6,7 @@ public class q12 {
 
         for (int i = 0; i < arr.length - 1; i ++) {
             int count = 0;
-            for (int j = i + 1; j < arr.length; j++) {
+            for (int j = i; j < arr.length; j++) {
                 if (arr[i] == arr[j]) {
                     count++;
                 }
