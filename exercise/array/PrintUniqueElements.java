7b23e6d47a364ac3bc6b9101f76cd16e24b6af61
tanzim12911 <tanzim12911@gmail.com>
2023-10-01 02:02:05 +0000

    Oct 1, 2023, 8:02 AM

--- exercise/array/PrintUniqueElements.java
+++ exercise/array/PrintUniqueElements.java
@@ -1,19 +1,30 @@
 package exercise.array;
 
+import java.util.Arrays;
+
 public class PrintUniqueElements {
     public static void main(String[] args) {
         int[] array = {3, 2, 2, 5};
-        UniqueElementsDisplay(array);
+        int[] result = UniqueElementsDisplay(array);
+
+        for(int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
     }
 
-    public static void UniqueElementsDisplay(int[] arr) {
+    public static int[] UniqueElementsDisplay(int[] arr) {
+        int index = 0;
+
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 1; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if(arr[i] != arr[j]) {
-                    System.out.println(arr[i] + " ");
+                    arr[index] = arr[i];
+                    index++;
                 }
             }
         }
+
+        return Arrays.copyOf(arr, index);
     }
 
     
