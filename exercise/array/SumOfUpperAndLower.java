b4c8bc9d07af7f4aeeb3bc70993d32832b7ae0f0
tanzim12911 <tanzim12911@gmail.com>
2023-10-01 16:26:10 +0000

    Oct 1, 2023, 10:26 PM

---
+++ exercise/array/SumOfUpperAndLower.java
@@ -0,0 +1,45 @@
+package exercise.array;
+
+import java.util.Scanner;
+
+public class SumOfUpperAndLower {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+
+        int[][] mat = new int[n][n];
+
+        System.out.println("Enter array elements: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        
+        int sumUpper = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j >= i; j--) {
+                sumUpper += mat[i][j];
+            }
+        }
+
+        System.out.println("Sum of upper triangle: " + sumUpper);
+
+        int sumLower = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                sumLower += mat[i][j];
+            }
+        }
+
+        System.out.println("Sum of upper triangle: " + sumUpper);
+
+
+
+    }
+
+}
