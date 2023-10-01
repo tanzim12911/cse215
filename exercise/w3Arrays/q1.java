ad99a9fb39ebd7dedfc23b2b229dcc90d1487219
tanzim12911 <tanzim12911@gmail.com>
2023-10-01 04:12:55 +0000

    Oct 1, 2023, 10:12 AM

--- exercise/w3Arrays/q1.java
+++ exercise/w3Arrays/q1.java
@@ -1,5 +1,7 @@
 package exercise.w3Arrays;
 
+import java.util.Arrays;
+
 public class q1 {
     public static void main(String[] args) {
         int[] arr1 = {1789, 2035, 1899, 1456, 2013, 1458, 2458, 1254, 1472, 2365, 1456, 2165, 1457, 2456};
@@ -9,7 +11,7 @@ public class q1 {
         }
         System.out.println();
 
-        sortNumericArr(arr1);
+        Arrays.sort(arr1);
         System.out.println("Sorted Numeric Array: ");
         for(int i = 0; i < arr1.length; i++) {
             System.out.print(arr1[i] + " ");
