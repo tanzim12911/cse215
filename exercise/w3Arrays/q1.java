d57e704f67bf2c494dba9764eb2f3d032a4bea2e
tanzim12911 <tanzim12911@gmail.com>
2023-10-01 04:16:54 +0000

    Oct 1, 2023, 10:16 AM

--- exercise/w3Arrays/q1.java
+++ exercise/w3Arrays/q1.java
@@ -21,7 +21,7 @@ public class q1 {
 
     public static void sortNumericArr(int[] array) {
     for(int i = 0; i < array.length - 1; i++) {
-        for(int j = 1; j < array.length; j++) {
+        for(int j = i + 1; j < array.length; j++) {
             if (array[i] > array[j]) {
             int temp = array[i];
             array[i] = array[j];
