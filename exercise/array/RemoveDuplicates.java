e6abdc9ed0c580a5249728a43813564bf75f0d1f
tanzim12911 <tanzim12911@gmail.com>
2023-10-01 04:10:22 +0000

    Oct 1, 2023, 10:10 AM

---
+++ exercise/array/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+package exercise.array;
+
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] array = {10, 22, 10, 20, 11, 22};
+
+    }
+
+    public static int[] RemoveDuplicatesMethod(int[] arr) {
+        Arrays.sort(arr);
+        
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == arr[2])
+        }
+    }
+
+
+}
---
+++ exercise/w3Arrays/q1.java
@@ -0,0 +1,31 @@
+package exercise.w3Arrays;
+
+public class q1 {
+    public static void main(String[] args) {
+        int[] arr1 = {1789, 2035, 1899, 1456, 2013, 1458, 2458, 1254, 1472, 2365, 1456, 2165, 1457, 2456};
+        System.out.println("Original Numeric Array: ");
+        for(int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i] + " ");
+        }
+
+        sortNumericArr(arr1);
+        System.out.println("Sorted Numeric Array: ");
+        for(int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i] + " ");
+        }
+    }
+    
+
+    public static void sortNumericArr(int[] array) {
+    for(int i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+            int temp = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
+        }
+    }
+}
+
+}
+
+
