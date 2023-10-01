d36aba9038cc25ab8d81fd7ad1de07924eed8542
tanzim12911 <tanzim12911@gmail.com>
2023-10-01 08:21:28 +0000

    Oct 1, 2023, 2:21 PM

---
+++ exercise/w3Arrays/q6.java
@@ -0,0 +1,24 @@
+package exercise.w3Arrays;
+
+public class q6 {
+    public static void main(String[] args) {
+        int[] arr = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+
+        int key = 2013;
+
+        System.out.println("Index of " + key + " is " + indexOfElement(arr, key));
+
+    }
+
+    public static int indexOfElement(int[] array, int n) {
+        int index = -1;
+        
+        for (int i = 0; i < array.length; i++) {
+            if (n == array[i]) {
+                index = i;
+            }
+        }
+
+        return index;
+    }
+}
