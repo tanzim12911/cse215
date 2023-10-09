cf4dd4cca9a8acde1aae82ed8edd3de8c9c6dd15
tanzim12911 <tanzim12911@gmail.com>
2023-10-09 08:19:29 +0000

    Oct 9, 2023, 2:19 PM

---
+++ exercise/TestFILEIO.java
@@ -0,0 +1,11 @@
+package exercise;
+
+import java.io.File;
+
+public class TestFILEIO {
+    public static void main(String[] args) {
+        File f1 = new File("testfile.txt");
+
+        System.out.println(f1.getAbsolutePath());
+    }
+}
