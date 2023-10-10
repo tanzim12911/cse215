53f738fd3f9aa6b19e499845a261014058f0459e
tanzim12911 <tanzim12911@gmail.com>
2023-10-10 16:51:02 +0000

    Oct 10, 2023, 10:51 PM

--- ch_13/q9/q9.java
+++ ch_13/q9/q9.java
@@ -2,8 +2,8 @@ package ch_13.q9;
 
 public class q9 {
     public static void main(String[] args) {
-        Circle c1 = new Circle(10);
-        Circle c2 = new Circle(10);
+        Circle c1 = new Circle(12.23);
+        Circle c2 = new Circle(67.89);
 
         System.out.println("C1 and C2 equals: " + c1.compareTo(c2));
 
