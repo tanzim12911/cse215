fdf1ac1aa1f980807bf66a8355689adcaa9a51b9
tanzim12911 <tanzim12911@gmail.com>
2023-10-10 13:37:23 +0000

    Oct 10, 2023, 7:37 PM

---
+++ ch_13/RequiredClass/Rectangle.java
@@ -0,0 +1,41 @@
+package ch_13.RequiredClass;
+
+public class Rectangle extends GeometricObject {
+    private double width;
+    private double height;
+
+    public Rectangle() {
+
+    }
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+    public Rectangle(double width, double height, String color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.height = height;
+    }
+    public double getWidth() {
+        return width;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+    
+}
