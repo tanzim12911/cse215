141c1aabd63af376a872d7052a81641ae0c495ec
tanzim12911 <tanzim12911@gmail.com>
2023-10-10 14:27:49 +0000

    Oct 10, 2023, 8:27 PM

--- ch_13/q6/ComparableCircle.java
+++ ch_13/q6/ComparableCircle.java
@@ -1,6 +1,8 @@
 package ch_13.q6;
 
 public class ComparableCircle extends Circle implements Comparable<Circle> {
-    
+    public int compareTo(Circle c) {
+        return Double.compare(super.getArea(), c.getArea());
+    }
     
 }
