bd0c53e7cf45038321b1db81f561fb30a415e5fb
tanzim12911 <tanzim12911@gmail.com>
2023-10-10 17:13:30 +0000

    Oct 10, 2023, 11:13 PM

---
+++ ch_13/q11/q11.java
@@ -0,0 +1,16 @@
+package ch_13.q11;
+
+public class q11 {
+    public static void main(String[] args) {
+        Octagon ot1 = new Octagon(5);
+
+        System.out.println("Area: " + ot1.getArea());
+        System.out.println("Perimeter: " + ot1.getPerimeter());
+
+        Octagon ot2 = null;
+        
+        try {
+            ot2 = ot1.clone();
+        }
+    }
+}
