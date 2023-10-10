7f4be67fbbb6e9f0fcac6efd8e54cf30b44f94a5
tanzim12911 <tanzim12911@gmail.com>
2023-10-10 13:37:25 +0000

    Oct 10, 2023, 7:37 PM

---
+++ ch_13/RequiredClass/Circle.java
@@ -0,0 +1,37 @@
+package ch_13.RequiredClass;
+
+public class Circle extends GeometricObject {
+    private double radius;
+
+    public Circle() {
+
+    }
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getDiameter(){
+        return 2 * radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    
+}
