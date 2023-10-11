efd94fb7af6a14435294ee8a58614d4d2260a52c
tanzim12911 <tanzim12911@gmail.com>
2023-10-11 06:54:44 +0000

    Oct 11, 2023, 12:54 PM

---
+++ ch_13/q13/Course.java
@@ -0,0 +1,42 @@
+package ch_13.q13;
+
+public class Course implements Cloneable {
+    
+    private String courseName;
+    private String[] students;
+    private int numberOfStudents;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void addStudent(String student) {
+        students[numberOfStudents] = student;
+        numberOfStudents++;
+    }
+
+    public void dropStudent(String name) {
+        for(int i = 0; i < students.length; i++) {
+            if (students[i].equals(name)) {
+                students[i] = null;
+            }
+        }
+    }
+
+    public String[] getStudents() {
+        return students;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    @Override
+    public Course clone() throws CloneNotSupportedException {
+        return (Course) super.clone();
+    }
+}
---
+++ ch_13/q13/q13.java
@@ -0,0 +1,31 @@
+package ch_13.q13;
+
+public class q13 {
+    public static void main(String[] args) {
+        Course c1 = new Course("CSE115");
+
+        c1.addStudent("Jinx");
+        c1.addStudent("Ekko");
+        c1.addStudent("Powder");
+        
+        System.out.println(c1.getCourseName());
+        System.out.println(c1.getNumberOfStudents());
+
+        String[] list = c1.getStudents();
+
+        for(String x: list) {
+            System.out.println(x);
+        }
+        
+        
+        c1.dropStudent("null");
+        
+        list = c1.getStudents();
+
+        for(String x: list) {
+            System.out.println(x);
+        }
+        
+    }
+
+}
