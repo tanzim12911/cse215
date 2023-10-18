4b56d6b9859a191706943348ede7d82e6fce908e
tanzim12911 <tanzim12911@gmail.com>
2023-10-18 02:34:47 +0000

    Oct 18, 2023, 8:34 AM

---
+++ ch_12/q5/TestClass.java
@@ -0,0 +1,14 @@
+package ch_12.q5;
+
+public class TestClass {
+    public static void main(String[] args) throws IllegalTriangleException {
+        try {
+            Triangle t = new Triangle(2, 2, 4);
+            System.out.println(t.toString());
+        } catch (Exception e) {
+            System.out.println("ERROR");
+        }
+
+    }
+}
+
