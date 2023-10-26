1b564f1bb585beb0933dceb21b3a1dd401b61d8f
tanzim12911 <tanzim12911@gmail.com>
2023-10-26 05:03:57 +0000

    Oct 26, 2023, 11:03 AM

---
+++ test.java
@@ -0,0 +1,18 @@
+class SuperClass
+{
+    protected int x = 0;
+    
+    public SuperClass(int x) {
+        this.x = x;
+    }
+    private void increment() {
+        x++; 
+    }
+    protected final void add(int y)
+    {
+        x += y;
+    }
+    public void display() {
+        System.out.println(x);
+    }
+}
