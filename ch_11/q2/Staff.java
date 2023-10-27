6c10f2fb17ab2c2024280e0533753787121becb4
tanzim12911 <tanzim12911@gmail.com>
2023-10-27 05:49:55 +0000

    Oct 27, 2023, 11:49 AM

--- ch_11/q2/Staff.java
+++ ch_11/q2/Staff.java
@@ -24,4 +24,11 @@ public class Staff extends Employee{
     public void setTitle(String title) {
         this.title = title;
     }
+
+    @Override
+    public String toString() {
+        return "Staff [title=" + title + "]";
+    }
+
+    
 }
