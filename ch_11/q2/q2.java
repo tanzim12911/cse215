7a38521913055590c56e1f1bbda433a374a1bc63
tanzim12911 <tanzim12911@gmail.com>
2023-10-27 06:04:13 +0000

    Oct 27, 2023, 12:04 PM

--- ch_11/q2/q2.java
+++ ch_11/q2/q2.java
@@ -5,10 +5,18 @@ import ch_10.q14.MyDate;
 public class q2 {
     public static void main(String[] args) {
         Person p = new Person("Rahim", "Uttara", 01711111111, "rarim@gmail.com");
-        Student s = new Student("Karim", "Mirpur", 01722222222, "karim@gmail.com", Student.FRESHMAN);
+        
+        Student s = new Student(Student.FRESHMAN);
+        s.setName("Karim");
+        
         Employee e = new Employee("ADM101", 25000, new MyDate());
-        Faculty f = new Faculty("SAC999", 150000, new MyDate(), "RA: 10:30AM", "Lecturer");
+        e.setName("Rafiq");
+        
+        Faculty f = new Faculty("SAC999", 250000, new MyDate(), "RA: 10:30AM", "Professor");
+        f.setName("Dr. Ahmed");
+
         Staff st = new Staff("ADM201", 50000, new MyDate(), "Accountant");
+        st.setName("Mr. Kabir");
 
         System.out.println(p.toString());
         System.out.println(s.toString());
