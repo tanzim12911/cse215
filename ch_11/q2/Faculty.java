9235155a8de12391fd3e35151350683274086df7
tanzim12911 <tanzim12911@gmail.com>
2023-10-27 05:47:22 +0000

    Oct 27, 2023, 11:47 AM

--- ch_11/q2/Faculty.java
+++ ch_11/q2/Faculty.java
@@ -1,8 +1,20 @@
 package ch_11.q2;
 
+import ch_10.q14.MyDate;
+
 public class Faculty extends Employee {
     private String officeHour;
     private String rank;
+
+    public Faculty(String office, double salary, MyDate dateHired, String officeHour, String rank) {
+        super(office, salary, dateHired);
+        this.officeHour = officeHour;
+        this.rank = rank;
+    }
+    public Faculty(String officeHour, String rank) {
+        this.officeHour = officeHour;
+        this.rank = rank;
+    }
 
     public String getOfficeHour() {
         return officeHour;
