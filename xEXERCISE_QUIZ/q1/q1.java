bfb2fd45dcf8891bdd1e87b341e924dc9ff88256
tanzim12911 <tanzim12911@gmail.com>
2023-10-29 14:44:09 +0000

    Oct 29, 2023, 8:44 PM

---
+++ xEXERCISE_QUIZ/q1/q1.java
@@ -0,0 +1,26 @@
+package xEXERCISE_QUIZ.q1;
+
+import java.io.IOException;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) throws IOException {
+        FileWriter file = new FileWriter("marksheet.txt", true);
+
+        try {
+            file.write("Tamim 29\n");
+            file.write("Shakib 75\n");
+            file.write("Mahmudullah 30\n");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Scanner read = new Scanner("marksheet.txt");
+
+        while (read.hasNext()) {
+            System.out.println(read.nextLine());
+        }
+    }
+}
