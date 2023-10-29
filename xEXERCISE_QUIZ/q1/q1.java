7ae4efbb802145c6b6e6c74b3ea7862469b7df86
tanzim12911 <tanzim12911@gmail.com>
2023-10-29 14:47:49 +0000

    Oct 29, 2023, 8:47 PM

--- xEXERCISE_QUIZ/q1/q1.java
+++ xEXERCISE_QUIZ/q1/q1.java
@@ -22,7 +22,7 @@ public class q1 {
 
         while (read.hasNext()) {
             String name = read.next();
-            double score = read.nextInt();
+            int score = read.nextInt();
 
             System.out.println("Name: " + name + "  Score: " + score);
         }
