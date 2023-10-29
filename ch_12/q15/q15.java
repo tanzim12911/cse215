7502202594d8488f2982a10fda4d0abc4d3fba39
tanzim12911 <tanzim12911@gmail.com>
2023-10-29 04:39:44 +0000

    Oct 29, 2023, 10:39 AM

---
+++ ch_12/q15/q15.java
@@ -0,0 +1,45 @@
+package ch_12.q15;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class q15 {
+    public static void main(String[] args) {
+        File f1 = new File("C:/Workspace/cse215/cse215/ch_12/q15" + "/Exercise12_15.txt");
+
+        try {
+            if(!f1.exists()) {
+            f1.createNewFile();
+        }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        try {
+            FileWriter f1w = new FileWriter(f1, true);
+
+            for (int i = 0; i < 100; i++) {
+                f1w.write((int) Math.random() * 100000 + " ");
+                f1w.close();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        try {
+            Scanner input = new Scanner(f1);
+
+            while(input.hasNext()) {
+                System.out.print(input.nextInt() + " ");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
