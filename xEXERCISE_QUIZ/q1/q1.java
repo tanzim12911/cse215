97401870fda96892ede0533f17c76dc289172aa5
tanzim12911 <tanzim12911@gmail.com>
2023-10-29 14:56:49 +0000

    Oct 29, 2023, 8:56 PM

--- xEXERCISE_QUIZ/q1/q1.java
+++ xEXERCISE_QUIZ/q1/q1.java
@@ -1,33 +1,29 @@
 package xEXERCISE_QUIZ.q1;
 
-import java.io.IOException;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
 
 public class q1 {
-    public static void main(String[] args) throws IOException {
-        FileWriter file = new FileWriter("marksheet.txt", true);
-
+    public static void main(String[] args) {
         try {
-            file.write("Tamim 29");
-            file.write("Shakib 75");
-            file.write("Mahmudullah 30");
+            FileWriter file = new FileWriter("marksheet.txt", true);
+            file.write("Tamim 29\n"); // Add a newline character after each entry
+            file.write("Shakib 75\n");
+            file.write("Mahmudullah 30\n");
             file.close();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        Scanner read = new Scanner(new File("/marksheet.txt"));
+            // Create a Scanner to read from the file
+            Scanner read = new Scanner(new File("marksheet.txt"));
 
-        try {
             while (read.hasNext()) {
-            String name = read.next();
-            int score = read.nextInt();
+                String name = read.next();
+                int score = read.nextInt();
 
-            System.out.println("Name: " + name + "  Score: " + score);
-        }
+                System.out.println("Name: " + name + "  Score: " + score);
+            }
+
+            read.close(); // Close the Scanner
         } catch (Exception e) {
             e.printStackTrace();
         }
