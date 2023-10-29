d7f1df514e65882e5547fcc64b252d471f095c01
tanzim12911 <tanzim12911@gmail.com>
2023-10-29 06:14:15 +0000

    Oct 29, 2023, 12:14 PM

---
+++ ch_17/q2/q2.java
@@ -0,0 +1,45 @@
+package ch_17.q2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class q2 {
+    public static void main(String[] args) throws IOException {
+        File f1 = new File("Exercise17_02.dat");
+
+        if (!f1.exists()) {
+            try {
+                f1.createNewFile();
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            FileOutputStream output = new FileOutputStream(f1, true);
+
+            for (int i = 0; i < 100; i++) {
+                int value = (int) (Math.random() * 200);
+
+                output.write(value);
+            }
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            FileInputStream input = new FileInputStream(f1);
+
+            while(input.read() != -1) {
+                System.out.println(input.read() + "");
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
