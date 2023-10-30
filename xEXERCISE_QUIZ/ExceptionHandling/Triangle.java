3bf8bb35e2d4be8b3d63504d98fc46be5bbbeeda
tanzim12911 <tanzim12911@gmail.com>
2023-10-30 06:44:06 +0000

    Oct 30, 2023, 12:44 PM

--- xEXERCISE_QUIZ/ExceptionHandling/Triangle.java
+++ xEXERCISE_QUIZ/ExceptionHandling/Triangle.java
@@ -6,9 +6,6 @@ public class Triangle {
     private double side3;
 
     Triangle() {
-        side1 = 1.0;
-        side2 = 1.0;
-        side3 = 1.0;
     }
 
     Triangle(double side1, double side2, double side3) throws IllegalTriangleException {
