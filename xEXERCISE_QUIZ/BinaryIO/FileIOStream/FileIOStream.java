72006fea0688be14d8f0d1e39723199cd108b0df
tanzim12911 <tanzim12911@gmail.com>
2023-10-30 03:53:29 +0000

    Oct 30, 2023, 9:53 AM

--- xEXERCISE_QUIZ/BinaryIO/FileIOStream/FileIOStream.java
+++ xEXERCISE_QUIZ/BinaryIO/FileIOStream/FileIOStream.java
@@ -1,5 +1,9 @@
 package xEXERCISE_QUIZ.BinaryIO.FileIOStream;
 
+import java.io.*;
+
 public class FileIOStream {
-    
+    public static void main(String[] args) throws IOException {
+        FileOutputStream output = new FileOutputStream("temp.dat");
+    }
 }
