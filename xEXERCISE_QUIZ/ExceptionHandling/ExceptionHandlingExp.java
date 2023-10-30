7b3009e7e02cccd854d691e4b34637dabbb953c4
tanzim12911 <tanzim12911@gmail.com>
2023-10-30 06:30:40 +0000

    Oct 30, 2023, 12:30 PM

--- xEXERCISE_QUIZ/ExceptionHandling/ExceptionHandlingExp.java
+++ xEXERCISE_QUIZ/ExceptionHandling/ExceptionHandlingExp.java
@@ -3,7 +3,7 @@ package xEXERCISE_QUIZ.ExceptionHandling;
 public class ExceptionHandlingExp {
     public static void main(String[] args) {
         try {
-            Triangle t1 = new Triangle(4, 5, 6);
+            Triangle t1 = new Triangle(4, 5, 2);
         }
         catch (IllegalTriangleException e) {
             e.printStackTrace();
