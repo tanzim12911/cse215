be5aeee4a6fdbc494abe253279657b934ee6b155
tanzim12911 <tanzim12911@gmail.com>
2023-10-30 06:36:07 +0000

    Oct 30, 2023, 12:36 PM

--- xEXERCISE_QUIZ/ExceptionHandling/Triangle.java
+++ xEXERCISE_QUIZ/ExceptionHandling/Triangle.java
@@ -15,9 +15,11 @@ public class Triangle {
         if (side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2) {
             throw new IllegalTriangleException("The sum of any two sides of a triangle must be greater than the other side");
         }
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
+        else {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        }
     }
 
     public double getSide1() {
