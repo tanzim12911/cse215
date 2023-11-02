96e37ae6f3de6123a4cebcccb7e5296b64ed3dca
tanzim12911 <tanzim12911@gmail.com>
2023-11-02 15:27:30 +0000

    Nov 2, 2023, 9:27 PM

--- lab14/Task2.java
+++ lab14/Task2.java
@@ -1,15 +1,30 @@
 package lab14;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Task2 {
     public static void main(String[] args) {
-        HashMap<String, Integer> squad = new HashMap<String, Integer>();
+        HashMap<String, String> squad = new HashMap<String, String>();
 
-        squad.put("Tamim", 29);
-        squad.put("Shakib", 75);
-        squad.put("Mustafizur", 90);
-        squad.put("Al Hasan", 75);
-        System.out.println(squad);
+        squad.put("Tamim", "29");
+        squad.put("Shakib", "75");
+        squad.put("Mustafizur", "90");
+        squad.put("Al Hasan", "75");
+
+        
+        Set<String> duplicateValues = new HashSet<>();
+
+        for (Map.Entry<String, String> entry : squad.entrySet()) {
+            if (duplicateValues.contains(entry.getValue())) {
+                System.out.println("Duplicate value: " + entry.getValue());
+            } else {
+                duplicateValues.add(entry.getValue());
+            }
+        }
+            
+        
     }
 }
