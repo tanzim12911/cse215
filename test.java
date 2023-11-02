8b83927e9e185ed6ede24f16c42d33d654d91abf
tanzim12911 <tanzim12911@gmail.com>
2023-11-02 15:41:38 +0000

    Nov 2, 2023, 9:41 PM

--- test.java
+++ test.java
@@ -1,38 +1,42 @@
-class SuperClass
-{
-    protected int x = 0;
-    
-    public SuperClass(int x) {
-        this.x = x;
-    }
-    private void increment() {
-        x++; 
-    }
-    protected final void add(int y) {
-        x += y;
-    }
-    public void display() {
-        System.out.println(x);
-    }
-}
+import java.util.ArrayList;
 
+public class ArrayListExample {
+    public static void main(String[] args) {
+        // Create an ArrayList and fill it with 10 items
+        ArrayList<String> myList = new ArrayList<>();
+        myList.add("Item1");
+        myList.add("Item2");
+        myList.add("Item3");
+        myList.add("Item4");
+        myList.add("Item5");
+        myList.add("Item2"); // Adding a duplicate item for the second occurrence
+        myList.add("Item6");
+        myList.add("Item7");
+        myList.add("Item8");
+        myList.add("Item9");
 
-public class SubClass extends SuperClass {
-    
-    public SubClass(int x) {
-        super(x); 
-    }
-    
-    public void display() {
-        add(2);
-        super.display();
-    }
+        // Check if the ArrayList contains a given element
+        String searchElement = "Item3";
+        if (myList.contains(searchElement)) {
+            System.out.println("ArrayList contains " + searchElement);
+        } else {
+            System.out.println("ArrayList does not contain " + searchElement);
+        }
 
-
-    public static void main(String [] args) {
-        SuperClass sc = new SuperClass(3);
-        sc.display();
-        sc = new SubClass(3);
-        sc.display();
+        // Find the index of the second occurrence of an element in the ArrayList
+        String targetElement = "Item2";
+        int firstIndex = myList.indexOf(targetElement);
+        if (firstIndex != -1) {
+            int secondIndex = myList.subList(firstIndex + 1, myList.size()).indexOf(targetElement);
+            if (secondIndex != -1) {
+                // Adjust the index to account for the sublist's offset
+                secondIndex += firstIndex + 1;
+                System.out.println("Index of the second occurrence of " + targetElement + " is " + secondIndex);
+            } else {
+                System.out.println("Second occurrence of " + targetElement + " not found in the ArrayList");
+            }
+        } else {
+            System.out.println(targetElement + " not found in the ArrayList");
+        }
     }
 }
