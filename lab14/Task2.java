444bd65b73aa1d710d4a114ca85db5dfbea1da8d
tanzim12911 <tanzim12911@gmail.com>
2023-11-02 12:27:57 +0000

    Nov 2, 2023, 6:27 PM

---
+++ lab14/Task2.java
@@ -0,0 +1,15 @@
+package lab14;
+
+import java.util.HashMap;
+
+public class Task2 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> squad = new HashMap<String, Integer>();
+
+        squad.put("Tamim", 29);
+        squad.put("Shakib", 75);
+        squad.put("Mustafizur", 90);
+        squad.put("Al Hasan", 75);
+        System.out.println(squad);
+    }
+}
