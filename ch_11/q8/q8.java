ac831d1ea66286fb31e4eecac7b8737e9561de8b
tanzim12911 <tanzim12911@gmail.com>
2023-11-19 12:44:15 +0000

    Nov 19, 2023, 6:44 PM

--- ch_11/q8/q8.java
+++ ch_11/q8/q8.java
@@ -14,9 +14,9 @@ public class q8 {
         ac.withdraw(4);
         ac.withdraw(2);
 
-        System.out.print("Name: " + ac.getName());
-        System.out.print("Interest Rate: " + ac.getAnnualInterestRate());
-        System.out.print("Balance: " + ac.getBalance());
+        System.out.println("Name: " + ac.getName());
+        System.out.println("Interest Rate: " + ac.getAnnualInterestRate());
+        System.out.println("Balance: " + ac.getBalance());
         System.out.println("Transaction Summary: ");
         System.out.println(ac.getTransactions());
         
