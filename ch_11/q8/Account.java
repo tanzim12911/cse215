3a72bf561dc5c46b5dcc9239f203e7e076cbd29a
tanzim12911 <tanzim12911@gmail.com>
2023-11-19 11:58:03 +0000

    Nov 19, 2023, 5:58 PM

---
+++ ch_11/q8/Account.java
@@ -0,0 +1,20 @@
+package ch_11.q8;
+
+import java.util.Date;
+
+public class Account {
+    private int id;
+    private double balance;
+    private double annualInterestRate;
+    private Date dateCreated;
+    
+    Account () {
+        id = 0;
+        balance = 0;
+        annualInterestRate = 0;
+    }
+
+    Account (int id, double balance) {
+        
+    }
+}
