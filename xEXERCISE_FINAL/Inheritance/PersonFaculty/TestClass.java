c0b48fedbcdba84e9b5ae615061f24803b433def
tanzim12911 <tanzim12911@gmail.com>
2023-11-21 09:59:45 +0000

    Nov 21, 2023, 3:59 PM

---
+++ xEXERCISE_FINAL/Inheritance/PersonFaculty/TestClass.java
@@ -0,0 +1,13 @@
+package xEXERCISE_FINAL.Inheritance.PersonFaculty;
+
+public class TestClass {
+    public static void main(String[] args) {
+        Faculty f = new Faculty("HSM", "Lecturer");
+
+        f.setName("Sahriar Hussain");
+        f.setAge(38);
+        f.setSalary(150000);
+
+        
+    }
+}
