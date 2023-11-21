efd8011c18de82884a2509c86f98ad34a6aab175
tanzim12911 <tanzim12911@gmail.com>
2023-11-21 11:41:25 +0000

    Nov 21, 2023, 5:41 PM

---
+++ xEXERCISE_FINAL/Abstraction/Shape/Traingle.java
@@ -0,0 +1,37 @@
+package xEXERCISE_FINAL.Abstraction.Shape;
+
+public class Traingle extends Shape {
+    private int base;
+    private int height;
+
+    Traingle() {
+
+    }
+
+    Traingle(int base, int height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public int getBase() {
+        return base;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    
+    @Override
+    public double getArea() {
+        return 0.5 * base * height;
+    }
+    
+}
