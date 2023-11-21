7f3d756947608c92a38aae707038c6634da9d2e3
tanzim12911 <tanzim12911@gmail.com>
2023-11-21 12:24:18 +0000

    Nov 21, 2023, 6:24 PM

---
+++ xEXERCISE_FINAL/Interface/PercentageDiscount.java
@@ -0,0 +1,13 @@
+package xEXERCISE_FINAL.Interface;
+
+public class PercentageDiscount implements Discountable {
+    private double percentage;
+
+    Public PercentageDiscount() {
+    }
+    public PercentageDiscount(double percentage) {
+        this.percentage = percentage;
+    }
+
+    
+}
