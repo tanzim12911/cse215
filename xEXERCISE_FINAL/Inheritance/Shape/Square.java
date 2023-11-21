935e43e0dbdcf0806e03e099495c38d38efdbec6
tanzim12911 <tanzim12911@gmail.com>
2023-11-21 09:48:11 +0000

    Nov 21, 2023, 3:48 PM

---
+++ xEXERCISE_FINAL/Inheritance/Shape/Square.java
@@ -0,0 +1,10 @@
+package xEXERCISE_FINAL.Inheritance.Shape;
+
+public class Square extends Rectangle {
+    Square() {
+    }
+    Square(double side1, double side2) {
+        super(side1, side2);
+    }
+    
+}
---
+++ xEXERCISE_FINAL/Inheritance/Shape/TestClass.java
@@ -0,0 +1,10 @@
+package xEXERCISE_FINAL.Inheritance.Shape;
+
+public class TestClass {
+    public static void main(String[] args) {
+        Square sq = new Square(10, 5);
+
+        System.out.println("Area: " + sq.area());
+        System.out.println("Perimeter: " + sq.perimeter());
+    }
+}
