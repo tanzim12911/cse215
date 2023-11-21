b8fc96d595ee1e7a0ee32dc022f84195e93dd1c1
tanzim12911 <tanzim12911@gmail.com>
2023-11-21 06:27:25 +0000

    Nov 21, 2023, 12:27 PM

---
+++ xEXERCISE_FINAL/Encapsulation/PointLine/TestClass.java
@@ -0,0 +1,11 @@
+package xEXERCISE_FINAL.Encapsulation.PointLine;
+
+public class TestClass {
+    public static void main(String[] args) {
+        Line l1 = new Line(4, 2, 8, 12);
+
+        System.out.println(l1.length());
+    }
+
+    
+}
