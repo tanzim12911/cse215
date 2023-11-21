04a707194f197797cea99635f3d4b7a3ec3ffa49
tanzim12911 <tanzim12911@gmail.com>
2023-11-21 08:23:09 +0000

    Nov 21, 2023, 2:23 PM

---
+++ xEXERCISE_FINAL/Inheritance/PersonEmployee/PartTimeEmployee.java
@@ -0,0 +1,45 @@
+package xEXERCISE_FINAL.Inheritance.PersonEmployee;
+
+public class PartTimeEmployee extends Employee {
+    private double hours;
+    private double rate;
+    
+    PartTimeEmployee() {
+    }
+    
+    PartTimeEmployee(double hours, double rate) {
+        this.hours = hours;
+        this.rate = rate;
+    }
+    PartTimeEmployee(String name, int age, String address, double hours, double rate) {
+        super(name, age, address);
+        this.hours = hours;
+        this.rate = rate;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "PartTimeEmployee [hours=" + hours + ", rate=" + rate + "]";
+    }
+
+    public double salary() {
+        return 15000;
+    }
+  
+}
