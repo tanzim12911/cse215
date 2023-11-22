5655a789e3a0fc00ed58abfd68d17bb71874441e
tanzim12911 <tanzim12911@gmail.com>
2023-11-22 10:26:02 +0000

    Nov 22, 2023, 4:26 PM

---
+++ xEXERCISE_FINAL/FileIO/TextIO/FileWriterExp/FileWriterExp.java
@@ -0,0 +1,26 @@
+package xEXERCISE_FINAL.FileIO.TextIO.FileWriterExp;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileWriterExp {
+    public static void main(String[] args) throws IOException {
+        File f1 = new File("marksheet.txt");
+
+        FileWriter fw = new FileWriter(f1);
+
+        fw.write("Van Dijk 89");
+        fw.write("Alisson Becker 90");
+        fw.write("Mo Salah 89");
+
+        Scanner read = new Scanner(f1);
+
+        while(read.hasNext()) {
+            System.out.println("First Name: " + read.next() + " | Last Name: " + read.next() + " | Score: " + read.nextInt());
+        }
+
+
+    }
+}
