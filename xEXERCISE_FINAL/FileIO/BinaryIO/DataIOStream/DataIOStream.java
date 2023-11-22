285e9c79b45b591bccec90982b769cdaa942bf3d
tanzim12911 <tanzim12911@gmail.com>
2023-11-22 11:03:59 +0000

    Nov 22, 2023, 5:03 PM

---
+++ xEXERCISE_FINAL/FileIO/BinaryIO/DataIOStream/DataIOStream.java
@@ -0,0 +1,34 @@
+package xEXERCISE_FINAL.FileIO.BinaryIO.DataIOStream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class DataIOStream {
+    public static void main(String[] args) throws IOException {
+        
+        try {
+            FileOutputStream output = new FileOutputStream("temp.dat");
+
+            for (int i = 1; i <= 10; i++) {
+                output.write(i);
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+
+        try {
+            FileInputStream input = new FileInputStream("temp.dat");
+
+            int value;
+
+            while ((value = input.read()) != -1) {
+                System.out.println(input.read() + " ");
+            }
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
