a636dc4ca66fc1e8fba9543f5063ba2f0de9468b
tanzim12911 <tanzim12911@gmail.com>
2023-11-22 11:17:34 +0000

    Nov 22, 2023, 5:17 PM

---
+++ xEXERCISE_FINAL/FileIO/BinaryIO/FileIOStream/FileIOStreamLoop.java
@@ -0,0 +1,35 @@
+package xEXERCISE_FINAL.FileIO.BinaryIO.FileIOStream;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileIOStreamLoop {
+    public static void main(String[] args) throws IOException{
+        FileOutputStream outfile = new FileOutputStream("temp1.dat");
+        DataOutputStream output = new DataOutputStream(outfile);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of Student: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Student " + i + " Name: ");
+            output.writeUTF(sc.nextLine());
+            output.writeInt(sc.nextInt());
+        }
+
+        FileInputStream infile = new FileInputStream("temp1.dat");
+        DataInputStream input = new DataInputStream(infile);
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Name: " + input.readUTF() + " | Score: " + input.readInt());
+        }
+        
+        
+        
+    }
+}
