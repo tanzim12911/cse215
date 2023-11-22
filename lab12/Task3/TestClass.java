7bf3f537620aa43d6e29d6ffbd3db2dd7c759095
tanzim12911 <tanzim12911@gmail.com>
2023-11-22 07:41:07 +0000

    Nov 22, 2023, 1:41 PM

--- lab12/Task3/TestClass.java
+++ lab12/Task3/TestClass.java
@@ -3,7 +3,7 @@ package lab12.Task3;
 public class TestClass {
     public static void main(String[] args) {
         try {
-            Triangle t = new Triangle(1.0, 0.1, 0.2);
+            Triangle t = new Triangle(1.0, 1.0, 1.0);
             System.out.println("Area: " + t.getArea());
         } catch (Exception e) {
             System.out.println(e.toString());
