6e82b71966077b3ccc66cfb5fe9183b03242952b
tanzim12911 <tanzim12911@gmail.com>
2023-11-22 12:13:19 +0000

    Nov 22, 2023, 6:13 PM

---
+++ lab13/Task2/Task2.java
@@ -0,0 +1,41 @@
+package lab13.Task2;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) throws IOException{
+        File f1 = new File("quizMarks");
+
+        FileWriter fw = new FileWriter(f1);
+
+        fw.write("113098 20\n");
+        fw.write("115089 15\n");
+        fw.write("345678 12\n");
+        fw.write("234566 18\n");
+
+        fw.close();
+
+        Scanner reader = new Scanner(f1);
+        
+        Quiz[] quiz = new Quiz[4];
+
+        int i = 0;
+
+        while (reader.hasNext()) {
+            int id = reader.nextInt();
+            int mark = reader.nextInt();
+
+            quiz[i].setId(id);
+            quiz[i].setMark(mark);
+
+            i++;
+        }
+
+        for (int i = 0; i < quiz.length; i++) {
+            
+        }
+    }
+}
