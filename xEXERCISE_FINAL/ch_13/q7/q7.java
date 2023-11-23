d333fe976c15eee65c4ce2fd2cdef107404d04e7
tanzim12911 <tanzim12911@gmail.com>
2023-11-23 13:02:11 +0000

    Nov 23, 2023, 7:02 PM

---
+++ xEXERCISE_FINAL/ch_13/q7/q7.java
@@ -0,0 +1,15 @@
+package xEXERCISE_FINAL.ch_13.q7;
+
+public class q7 {
+    public static void main(String[] args) {
+        GeometricObject[] gmtObj = new GeometricObject[5];
+
+        for (int i = 0; i < gmtObj.length; i++) {
+            gmtObj[i] = new Square(Math.random() * 100);
+        }
+
+        for (int j = 0; j < gmtObj.length; j++) {
+            
+        }
+    }
+}
