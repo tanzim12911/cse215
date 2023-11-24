7b91935cf3b66d799509ce1debba9093795aa36a
tanzim12911 <tanzim12911@gmail.com>
2023-11-24 15:31:14 +0000

    Nov 24, 2023, 9:31 PM

--- xEXERCISE_FINAL/GUI/Frame/FrameTest.java
+++ xEXERCISE_FINAL/GUI/Frame/FrameTest.java
@@ -10,6 +10,9 @@ public class FrameTest {
         f1.setSize(600, 400);
         f1.setVisible(true);
         f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f1.setBackground(new Color(500));
+        
+
+        JButton jb1 = new JButton("Press");
+        f1.add(jb1);
     }
 }
