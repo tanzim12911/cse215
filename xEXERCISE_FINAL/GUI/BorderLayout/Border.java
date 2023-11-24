cd3a42bfdf9d8e352f2e5c9b93cee5667e7ee060
tanzim12911 <tanzim12911@gmail.com>
2023-11-24 15:59:48 +0000

    Nov 24, 2023, 9:59 PM

---
+++ xEXERCISE_FINAL/GUI/BorderLayout/Border.java
@@ -0,0 +1,34 @@
+package xEXERCISE_FINAL.GUI.BorderLayout;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridLayout;
+
+import javax.swing.*;
+
+public class Border extends JFrame {
+    private Container x;
+
+    Border() {
+        allComponant();
+    }
+
+    void allComponant() {
+        x = this.getContentPane();
+
+        GridLayout gl = new GridLayout(2, 3, 10, 10);
+
+        x.setLayout(gl);
+
+        x.setBackground(Color.RED);
+
+        JLabel jl1 = new JLabel("Name: ");
+        x.add(jl1);
+        JTextField jt1 = new JTextField(24);
+
+        JLabel jl2 = new JLabel("ID: ");
+        x.add(jl2);
+        JTextField jt2 = new JTextField(24);
+
+    }
+} 
