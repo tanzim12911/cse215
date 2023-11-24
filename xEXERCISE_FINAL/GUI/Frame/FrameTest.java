ab20336a479998d30c381e6e1569caf2d0cbaf23
tanzim12911 <tanzim12911@gmail.com>
2023-11-24 15:26:51 +0000

    Nov 24, 2023, 9:26 PM

---
+++ xEXERCISE_FINAL/GUI/Frame/FrameTest.java
@@ -0,0 +1,15 @@
+package xEXERCISE_FINAL.GUI.Frame;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class FrameTest {
+    public static void main(String[] args) {
+        Frame f1 = new Frame("Test Frame");
+
+        f1.setSize(400, 200);
+        f1.setVisible(true);
+    }
+
+    
+}
