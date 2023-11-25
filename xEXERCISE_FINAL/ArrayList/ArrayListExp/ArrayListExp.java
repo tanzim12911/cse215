0558f185234342c2ff8bf46bc0bcba6dafa469b4
tanzim12911 <tanzim12911@gmail.com>
2023-11-25 03:32:41 +0000

    Nov 25, 2023, 9:32 AM

---
+++ xEXERCISE_FINAL/ArrayList/ArrayListExp/ArrayListExp.java
@@ -0,0 +1,17 @@
+package xEXERCISE_FINAL.ArrayList.ArrayListExp;
+
+import java.util.ArrayList;
+
+public class ArrayListExp {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("London");
+        list.add("Denver");
+        list.add("Paris");
+        list.add("Miami");
+        list.add("Seoul");
+
+        System.out.println(list.toString());
+    }
+}
