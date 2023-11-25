77dc2d5be6fed1a6e4c2ebeb613bb2c01ed22c26
tanzim12911 <tanzim12911@gmail.com>
2023-11-25 04:52:08 +0000

    Nov 25, 2023, 10:52 AM

---
+++ xEXERCISE_FINAL/HashSet/HashSetExp.java
@@ -0,0 +1,16 @@
+package xEXERCISE_FINAL.HashSet;
+
+import java.util.HashSet;
+
+public class HashSetExp {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+
+        set.add("London");
+        set.add("Paris");
+        set.add("Denver");
+        set.add("Seoul");
+
+        System.out.println(set.toString());
+    }
+}
