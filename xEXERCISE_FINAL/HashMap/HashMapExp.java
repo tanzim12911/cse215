3081bd9ad0d8ef5caf95f88a88dc70f90c9aea2c
tanzim12911 <tanzim12911@gmail.com>
2023-11-25 05:01:59 +0000

    Nov 25, 2023, 11:01 AM

---
+++ xEXERCISE_FINAL/HashMap/HashMapExp.java
@@ -0,0 +1,22 @@
+package xEXERCISE_FINAL.HashMap;
+
+import java.util.HashMap;
+
+public class HashMapExp {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("Shakib", 80);
+        map.put("Tamim", 79);
+        map.put("Miraz", 72);
+
+        System.out.println(map.get("Shakib"));
+
+        for(String i : map.keySet()) {
+            System.out.println(i);
+        }
+        for(Integer i : map.values()) {
+            System.out.println(i);
+        }
+    }
+}
