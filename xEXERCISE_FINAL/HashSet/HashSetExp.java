083feff3b2f612e34916f9c9010a706af92a3f50
tanzim12911 <tanzim12911@gmail.com>
2023-11-25 04:53:48 +0000

    Nov 25, 2023, 10:53 AM

--- xEXERCISE_FINAL/HashSet/HashSetExp.java
+++ xEXERCISE_FINAL/HashSet/HashSetExp.java
@@ -17,8 +17,8 @@ public class HashSetExp {
 
         System.out.println(set.toString());
 
-        set.contains("Seoul");
+        
 
-        System.out.println(set.toString());
+        System.out.println(set.contains("Seoul"));
     }
 }
