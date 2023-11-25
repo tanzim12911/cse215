e191c28b2bd201f4bce35aa3bade1a4403cc1450
tanzim12911 <tanzim12911@gmail.com>
2023-11-25 03:50:08 +0000

    Nov 25, 2023, 9:50 AM

---
+++ xEXERCISE_FINAL/ArrayList/ArrayListConversionExp/ArrayListConversionExp2.java
@@ -0,0 +1,15 @@
+package xEXERCISE_FINAL.ArrayList.ArrayListConversionExp;
+
+import java.util.ArrayList;
+
+public class ArrayListConversionExp2 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Van Dijk");
+        list.add("Alisson BEcker");
+        list.add("Mo Salah");
+
+        System.out.println(list.toString());
+    }
+}
